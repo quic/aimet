26be31faa95052023d31432f095408736f3b6d01
Sangeetha Siddegowda <quic_ssiddego@quicinc.com>
2020-06-26 12:33:57 -0700

    refactor to create common utilties for min max variable read and delta offset computation
    Signed-off-by: Sangeetha Siddegowda <quic_ssiddego@quicinc.com>

---
+++ TrainingExtensions/common/src/python/aimet_common/quantsim.py
@@ -0,0 +1,74 @@
+# /usr/bin/env python3.5
+# -*- mode: python -*-
+# =============================================================================
+#  @@-COPYRIGHT-START-@@
+#
+#  Copyright (c) 2020, Qualcomm Innovation Center, Inc. All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+#  3. Neither the name of the copyright holder nor the names of its contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+#  @@-COPYRIGHT-END-@@
+# =============================================================================
+
+""" Common utility for Quantization """
+import math
+
+
+def gate_min_max(min_val: float, max_val: float)-> (float, float):
+    """
+    Gates min and max encoding values to retain zero in the range representation.
+    Rules : min at maximum can be zero, max at minimum can be zero and
+    if max and min are equal, adds epsilon to maintain range.
+    :param min_val: min encoding value
+    :param max_val: max encoding value
+    :return: gated min and max values
+    """
+
+    epsilon = 1e-5
+    gated_min = min(min_val, 0.0)
+    gated_max = max(max_val, 0.0)
+    gated_max = max(gated_max, gated_min + epsilon)
+
+    return gated_min, gated_max
+
+
+def calculate_delta_offset(min_val: float, max_val: float, bitwidth: int)-> (float, float):
+    """
+    calculates delta and offset given min and max.
+    :param min_val: min encoding value
+    :param max_val: max encoding value
+    :param bitwidth: bitwidth used for quantization
+    :return: delta and offset values computed
+    """
+
+    delta = (max_val - min_val) / (2 ** bitwidth - 1)
+    if delta == 0:
+        delta = 1e-5
+    offset = math.floor(-min_val / delta)
+    return delta, offset
