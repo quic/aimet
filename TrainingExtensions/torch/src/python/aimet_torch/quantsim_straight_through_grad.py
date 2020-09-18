bd38dd8e22be54192beea706b198a0f7a861ebff
Sangeetha Siddegowda <quic_ssiddego@quicinc.com>
2020-09-18 15:01:19 -0700

    add ste grad math on torch

---
+++ TrainingExtensions/torch/src/python/aimet_torch/quantsim_straight_through_grad.py
@@ -0,0 +1,61 @@
+# /usr/bin/env python3.5
+# -*- mode: python -*-
+# =============================================================================
+#  @@-COPYRIGHT-START-@@
+#
+#  Copyright (c) 2020, Qualcomm Innovation Center, Inc. All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+#  3. Neither the name of the copyright holder nor the names of its contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+#  @@-COPYRIGHT-END-@@
+# =============================================================================
+""" implements straight through gradient computation for Quant op"""
+
+import torch
+
+
+def compute_dloss_by_dx(x, grad, encoding_min, encoding_max):
+    """
+    compute derivative w.r.t input uing straiht through estimator.
+    :param grad: gradient flowing
+    :param encoding_min: encoding min grid param used on forward pass
+    :param encoding_max: encoding max grid param used on forward pass
+    :return: gradient w.r.t input
+    """
+
+    # compute dloss_by_dx = dq_by_dx * grad
+    inner_cond = torch.where(torch.le(x, encoding_max),  # condition to check per value
+                             torch.ones_like(x),  # execute if true
+                             torch.zeros_like(x))  # execute if false
+
+    dloss_by_dx = (torch.where(torch.le(encoding_min, x),  # condition to check per value
+                               inner_cond,  # execute if true
+                               torch.zeros_like(x))) * grad
+
+    return dloss_by_dx
