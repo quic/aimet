07a9ccc2bd73f243eed24f971f3cd080f385b99b
Kevin Hsieh <64552835+quic-klhsieh@users.noreply.github.com>
2020-09-29 15:35:59 -0700

    Make ops without encodings passthrough after compute encodings

--- TrainingExtensions/tensorflow/src/python/aimet_tensorflow/quantsim.py
+++ TrainingExtensions/tensorflow/src/python/aimet_tensorflow/quantsim.py
@@ -527,26 +527,45 @@ class QuantizationSimModel:
         # Run data through the quantsim so we can compute activation encodings
         forward_pass_callback(self.session, forward_pass_callback_args)
 
+        ops_with_invalid_encodings = []
+
         # For activations, calculate encodings and update min-max parameters
         for op_name, quantizer_info in self._activation_quantizers.items():
-
             # Calculate encodings
             if quantizer_info.get_op_mode() != int(libpymo.TensorQuantizerOpMode.passThrough):
                 op_bitwidth, op_use_symmetric_encodings = self._get_bitwidth_and_symmetric_flag(
                     quantizer_info.quant_op_name)
                 encoding = quantizer_info.tensor_quantizer.computeEncoding(op_bitwidth, op_use_symmetric_encodings)
-                self._set_op_input_variables(op_name, encoding, libpymo.TensorQuantizerOpMode.quantizeDequantize)
+                if quantizer_info.tensor_quantizer.isEncodingValid:
+                    self._set_op_input_variables(op_name, encoding, libpymo.TensorQuantizerOpMode.quantizeDequantize)
+                else:
+                    self._set_op_input_variables(op_name, encoding, libpymo.TensorQuantizerOpMode.passThrough)
+                    ops_with_invalid_encodings.append(op_name)
 
         # For post-training mode, params will always be in one-shot mode
         op_mode = QuantizationSimModel._param_op_mode_after_analysis(self._quant_scheme)
 
         for op_name, quantizer_info in self._param_quantizers.items():
-
             if quantizer_info.get_op_mode() != int(libpymo.TensorQuantizerOpMode.passThrough):
                 op_bitwidth, op_use_symmetric_encodings = self._get_bitwidth_and_symmetric_flag(
                     quantizer_info.quant_op_name)
                 encoding = quantizer_info.tensor_quantizer.computeEncoding(op_bitwidth, op_use_symmetric_encodings)
-                self._set_op_input_variables(op_name, encoding, op_mode)
+                if quantizer_info.tensor_quantizer.isEncodingValid:
+                    self._set_op_input_variables(op_name, encoding, op_mode)
+                else:
+                    self._set_op_input_variables(op_name, encoding, libpymo.TensorQuantizerOpMode.passThrough)
+                    ops_with_invalid_encodings.append(op_name)
+
+        if ops_with_invalid_encodings:
+            _logger.info('The following quantizers did not have valid encodings and have been set to passThrough mode: '
+                         '%s', ops_with_invalid_encodings)
+            _logger.info('This can be due to the quantizers not having been evaluated during the forward pass in '
+                         'compute encodings. Evaluation is required to collect statistics needed to compute valid '
+                         'encodings.\n'
+                         'As a result, the quantizers have been set to passThrough mode, meaning no quantization noise '
+                         'will be simulated for these ops if they are evaluated in the future.\n'
+                         'If this is not desired, amend the forward pass to evaluate tensors which require these ops '
+                         'to be evaluated, and recompute encodings.')
 
     def export(self, path: str, filename_prefix: str, orig_sess: tf.Session = None):
         """
