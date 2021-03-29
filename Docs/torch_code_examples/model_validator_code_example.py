6a12e1e3c8b6f7894ff4255bbd41bac97eaed488
Kevin Hsieh <quic_klhsieh@quicinc.com>
2021-03-29 13:32:31 -0700

    Add code example for model validator utility in api docs
    Signed-off-by: Kevin Hsieh <quic_klhsieh@quicinc.com>

---
+++ Docs/torch_code_examples/model_validator_code_example.py
@@ -0,0 +1,60 @@
+# /usr/bin/env python3.5
+# -*- mode: python -*-
+# =============================================================================
+#  @@-COPYRIGHT-START-@@
+#
+#  Copyright (c) 2021, Qualcomm Innovation Center, Inc. All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+#  3. Neither the name of the copyright holder nor the names of its contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+#  @@-COPYRIGHT-END-@@
+# =============================================================================
+# pylint: disable=missing-docstring
+""" These are code examples to be used when generating AIMET documentation via Sphinx """
+
+import torch
+
+# Both compression and quantization related imports
+from aimet_torch.examples import mnist_torch_model
+
+# Model Validator related imports
+from aimet_torch.model_validator.model_validator import ModelValidator
+
+
+def validate_mnist_model():
+
+    # Load a trained MNIST model
+    model = mnist_torch_model.Net()
+
+    # Output of ModelValidator.validate_model will be True if model is valid, False otherwise
+    print(ModelValidator.validate_model(model, model_input=torch.rand(1, 1, 28, 28)))
+
+
+if __name__ == '__main__':
+    validate_mnist_model()
