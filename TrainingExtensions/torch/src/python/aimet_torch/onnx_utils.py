23795805f032e4d94928ad5312d94f7ee1cdd087
Abhi Khobare <quic_akhobare@quicinc.com>
2021-04-05 11:31:20 -0700

    Changed the way torch quantize, dequantize were registered for onnx export - to workaround an intermittent memory errors that subsequently showed up
    Signed-off-by: Abhi Khobare <quic_akhobare@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/onnx_utils.py
+++ TrainingExtensions/torch/src/python/aimet_torch/onnx_utils.py
@@ -36,6 +36,85 @@
 #  @@-COPYRIGHT-END-@@
 # =============================================================================
 
+# =============================================================================
+#  @@-COPYRIGHT-START-@@
+# From PyTorch:
+#
+# Copyright (c) 2016-     Facebook, Inc            (Adam Paszke)
+# Copyright (c) 2014-     Facebook, Inc            (Soumith Chintala)
+# Copyright (c) 2011-2014 Idiap Research Institute (Ronan Collobert)
+# Copyright (c) 2012-2014 Deepmind Technologies    (Koray Kavukcuoglu)
+# Copyright (c) 2011-2012 NEC Laboratories America (Koray Kavukcuoglu)
+# Copyright (c) 2011-2013 NYU                      (Clement Farabet)
+# Copyright (c) 2006-2010 NEC Laboratories America (Ronan Collobert, Leon Bottou, Iain Melvin, Jason Weston)
+# Copyright (c) 2006      Idiap Research Institute (Samy Bengio)
+# Copyright (c) 2001-2004 Idiap Research Institute (Ronan Collobert, Samy Bengio, Johnny Mariethoz)
+#
+# From Caffe2:
+#
+# Copyright (c) 2016-present, Facebook Inc. All rights reserved.
+#
+# All contributions by Facebook:
+# Copyright (c) 2016 Facebook Inc.
+#
+# All contributions by Google:
+# Copyright (c) 2015 Google Inc.
+# All rights reserved.
+#
+# All contributions by Yangqing Jia:
+# Copyright (c) 2015 Yangqing Jia
+# All rights reserved.
+#
+# All contributions by Kakao Brain:
+# Copyright 2019-2020 Kakao Brain
+#
+# All contributions from Caffe:
+# Copyright(c) 2013, 2014, 2015, the respective contributors
+# All rights reserved.
+#
+# All other contributions:
+# Copyright(c) 2015, 2016 the respective contributors
+# All rights reserved.
+#
+# Caffe2 uses a copyright model similar to Caffe: each contributor holds
+# copyright over their contributions to Caffe2. The project versioning records
+# all such contribution and copyright details. If a contributor wants to further
+# mark their specific copyright on a particular contribution, they should
+# indicate their copyright solely in the commit message of the change when it is
+# committed.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the names of Facebook, Deepmind Technologies, NYU, NEC Laboratories America
+#    and IDIAP Research Institute nor the names of its contributors may be
+#    used to endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#  @@-COPYRIGHT-END-@@
+# =============================================================================
+
+
 """ Utilities to load and save onnx models """
 
 from typing import Union, List, Tuple, Dict
@@ -101,6 +180,29 @@ onnx_pytorch_conn_graph_type_pairs = [
     [["Dropout"], ["dropout"]]
 ]
 
+import torch.onnx.symbolic_registry as sym_registry
+import importlib
+from inspect import getmembers, isfunction
+
+"""
+Copied and modified code for torch/onnx/symbolic_caffe2.py
+This is a temporary measure - Calling this function as-is was causing unexplained memory errors later on
+This can perhaps be replaced by registering the marker layers as custom ONNX ops
+"""
+def register_quantized_ops(domain, version):
+    # Register all the non-quantized ops
+    sym_registry.register_version('', version)
+    # Register all quantized ops
+    module = importlib.import_module('torch.onnx.symbolic_caffe2')
+    sym_registry._symbolic_versions['caffe2'] = module
+    quant_version_ops = getmembers(sym_registry._symbolic_versions['caffe2'])
+    for op in quant_version_ops:
+        if isfunction(op[1]) and not sym_registry.is_registered_op(op[0], domain, version):
+            aten_q_ops = ['dequantize', 'quantize_per_tensor']
+            if op[0] in aten_q_ops:
+                sym_registry.register_op(op[0], op[1], '', version)
+
+register_quantized_ops('caffe2', 9)
 
 
 class OnnxSaver:
@@ -242,8 +344,6 @@ class OnnxSaver:
 
         model = copy.deepcopy(pt_model).cpu()
         cls._add_markers(model)
-
-        torch.onnx.symbolic_caffe2.register_quantized_ops('caffe2', 9)
 
         temp_file = os.path.join(working_dir, 'temp_onnx_model_with_markers.onnx')
         torch.onnx.export(model, dummy_input, temp_file)
