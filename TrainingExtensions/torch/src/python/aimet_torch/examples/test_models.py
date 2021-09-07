c23a6e6c57e2162415a8bfa6ed6201946e903325
Hitarth Mehta <quic_hitameht@quicinc.com>
2021-09-07 17:39:30 +0530

    Add logic to replace functional by modules using torch FX
    Signed-off-by: Hitarth Mehta <quic_hitameht@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/examples/test_models.py
+++ TrainingExtensions/torch/src/python/aimet_torch/examples/test_models.py
@@ -539,6 +539,8 @@ class ConfigurableTupleOutputModel(torch.nn.Module):
         c2 = self.conv2(inputs[1])
         c3 = self.conv3(inputs[2])
         return c1, c2, c3
+
+
 class SingleLayerRNNModel(nn.Module):
     """
     Model using torch.nn.RNN module
@@ -651,4 +653,54 @@ class NestedSequentialModel(nn.Module):
         x = self.seq_list(inputs[0])
         x = x.view(x.size(0), -1)
         x = self.fc(x)
+        return x
+
+
+class ModelWithFunctionalReLU(nn.Module):
+    """ Model that uses functional ReLU instead of nn.Modules. Expects input of shape (1, 3, 32, 32) """
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x).relu()
+        x = self.fc3(x)
+        return x
+
+
+class ModelWithDuplicateReLU(nn.Module):
+    """ Model that uses single ReLU instances multiple times in the forward. Expects input of shape (1, 3, 32, 32) """
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool2(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x).relu()
+        x = self.fc3(x)
         return x
