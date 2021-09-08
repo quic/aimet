859895a8e641d47ce2dfcd43ac7a663279d7459a
Dipika Khullar <quic_dkhullar@quicinc.com>
2021-09-08 14:53:58 -0700

    fixing typo in recommended

--- Examples/torch/compression/spatial_svd_cp.py
+++ Examples/torch/compression/spatial_svd_cp.py
@@ -208,7 +208,7 @@ def aimet_channel_pruning(model: torch.nn.Module,
                                                                           modules_to_ignore=[model.conv1])
 
     # configure the parameters for channel pruning compression
-    # 50000 reconstruction samples will give better results and is reccommended; however we use 5000 here as an example.
+    # # 50000 reconstruction samples will give better results and is recommended; however we use 5000 here as an example.
     params = aimet_torch.defs.ChannelPruningParameters(data_loader= data_loader,
                                                       num_reconstruction_samples=5000,
                                                       allow_custom_downsample_ops=False,
