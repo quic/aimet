Pytorch range learning gradient computation fix
Signed-off-by: Geunho Lee <quic_geunlee@quicinc.com>

TrainingExtensions/torch/src/python/aimet_torch/quantsim_straight_through_grad.py
@@ -36,8 +36,34 @@
 #  @@-COPYRIGHT-END-@@
 # =============================================================================
 """ Implements straight through gradient computation for Quant op"""
+from dataclasses import dataclass
 
 import torch
+
+
+@dataclass
+class LearnedGridParams:
+    """
+    Data carrier containing parameters for learned grid
+    """
+    scaling: torch.Tensor
+    offset: torch.Tensor
+    n: torch.Tensor
+    p: torch.Tensor
+
+
+@dataclass
+class IntermediateResultForLearnedGrid:
+    """
+    Data carrier containing intermediate result for learned grid backward computation
+
+    forward_result: Round(x / scaling) + Round(offset)
+    rounding_error_q: Round(x / scaling) - (x / scaling)
+    rounding_error_o: Round(offset) - offset
+    """
+    forward_result: torch.Tensor
+    rounding_error_q: torch.Tensor
+    rounding_error_o: torch.Tensor
 
 
 def broadcast_to_tensor(tensor, encoding, ch_axis):
@@ -101,65 +127,139 @@ def compute_dloss_by_dx(x, grad, encoding_min, encoding_max, ch_axis=0):
     return dloss_by_dx
 
 
-def compute_dloss_by_dmin_using_dmax(dloss_by_dmax):
+def _compute_derivative_of_loss_function(x: torch.Tensor,
+                                         derivative_of_quantizer: torch.Tensor,
+                                         grad: torch.Tensor,
+                                         scaling: torch.Tensor,
+                                         ch_axis: int) -> torch.Tensor:
     """
-    compute derivative of loss w.r.t min, it is sign flipped version of derivative w.r.t max
-    :param dloss_by_dmax derivative w.r.t max
-    :return: derivative of loss w.r.t min
-    """
+    Compute derivative of the loss function like dloss_by_dmin or dloss_by_dmax
 
-    return -1 * dloss_by_dmax
-
-def compute_dloss_by_dmax(x, grad, scaling, offset, n, p, ch_axis=0):
-    """
-    helper function to compute derivative of loss w.r.t encoding max
     :param x: input
+    :param derivative_of_quantizer: derivative of the quantizer function like dq_by_dmin or dq_by_dmax
     :param grad: gradient
     :param scaling: scaling factor computed for given encoding min/max
-    :param offset: offset computed
-    :param n: lower bound
-    :param p: upper bound
     :param ch_axis: channel axis along which sum is computed for gradient calculation
-    :return: computed derivative of loss w.r.t encoding max
+    :return: computed derivative of loss w.r.t derivative of quantizer
     """
-
-    r_x_by_s_plus_round_o = torch.round(x / scaling) + torch.round(offset)
-    # R(x/s)-(x/s)
-    r_x_by_s_minus_x_by_s = torch.round(x / scaling) - (x / scaling)
-
-    # compute dq_by_dmax
-    # expr to be used if r_x_by_s_plus_round_o < n or > p
-    false_expr = torch.clamp(r_x_by_s_plus_round_o.data, min=n.data.item(), max=p.data.item()) * (1 / p)
-    inner_cond = torch.where(torch.le(r_x_by_s_plus_round_o.data, p), (r_x_by_s_minus_x_by_s * (1 / p)), false_expr)
-
-    # we need a scalar value for dq_by_dmax, so reduce 4d value computed above
-    # to single value before returning gradient
-    # this uses chain rule, multiply by loss and sum it to get scalar.
-    dq_by_dmax = torch.where(torch.le(n, r_x_by_s_plus_round_o.data), inner_cond, false_expr)
-
-    dloss_by_dmax = dq_by_dmax * grad
+    derivative_of_loss_function = derivative_of_quantizer * grad
     if len(scaling) > 1 and len(x.shape) > 1:
         dim = list(range(len(x.shape)))
         # Remove the output axis
         dim.pop(ch_axis)
-        dloss_by_dmax = torch.sum(dloss_by_dmax, dim=dim)
+        derivative_of_loss_function = torch.sum(derivative_of_loss_function, dim=dim)
     elif len(scaling) == 1:
-        dloss_by_dmax = torch.sum((dloss_by_dmax).flatten(), dim=0, keepdim=True)
+        derivative_of_loss_function = torch.sum(derivative_of_loss_function.flatten(), dim=0, keepdim=True)
 
+    return derivative_of_loss_function
+
+
+def compute_intermediate_result_for_learned_grid(x: torch.Tensor,
+                                                 scaling: torch.Tensor,
+                                                 offset: torch.Tensor) -> IntermediateResultForLearnedGrid:
+    """
+    helper function to compute forward result and rounding error before derivative
+    :param x: input
+    :param scaling: scaling factor computed for given encoding min/max
+    :param offset: offset computed
+    :return: forward result, rounding error of quantizer, rounding error of offset tuple
+    """
+    forward_result = torch.round(x / scaling) + torch.round(offset)
+    rounding_error_q = torch.round(x / scaling) - (x / scaling)
+    rounding_error_o = torch.round(offset) - offset
+
+    return IntermediateResultForLearnedGrid(forward_result, rounding_error_q, rounding_error_o)
+
+
+def compute_dloss_by_dmin(x: torch.Tensor,
+                          grad: torch.Tensor,
+                          intermediate_result: IntermediateResultForLearnedGrid,
+                          grid_params: LearnedGridParams,
+                          ch_axis: int = 0) -> torch.Tensor:
+    """
+    helper function to compute derivative of loss w.r.t encoding min
+    Implementation based on LSQ+ ( https://arxiv.org/pdf/2004.09576.pdf )
+
+    Inner condition ( n <= fw <= p ):
+        dq_by_dmin = (round(x/s) - x/s) / -p
+    Outer condition ( fw < n ):
+        dq_by_dmin = -n/p + 1 + (round(o) - o)/p
+    Outer condition ( p < fw ):
+        dq_by_dmin = (round(o) - o)/p
+
+    :param x: input
+    :param grad: gradient
+    :param intermediate_result: data carrier containing intermediate result (forward result, rounding error q and o)
+    :param grid_params: data carrier containing parameters for learned grid (scale, offset, n, p)
+    :param ch_axis: channel axis along which sum is computed for gradient calculation
+    :return: computed derivative of loss w.r.t encoding min
+    """
+    scaling, _, n, p = grid_params.scaling, grid_params.offset, grid_params.n, grid_params.p
+    forward_result = intermediate_result.forward_result
+    rounding_error_q = intermediate_result.rounding_error_q
+    rounding_error_o = intermediate_result.rounding_error_o
+
+    dq_by_dmin = torch.where(
+        torch.le(forward_result.data, p), -rounding_error_q / p, rounding_error_o / p
+    )
+    dq_by_dmin = torch.where(
+        torch.le(n, forward_result.data), dq_by_dmin, -n/p + 1 + rounding_error_o / p
+    )
+
+    dloss_by_dmin = _compute_derivative_of_loss_function(x, dq_by_dmin, grad, scaling, ch_axis)
+    return dloss_by_dmin
+
+
+def compute_dloss_by_dmax(x: torch.Tensor,
+                          grad: torch.Tensor,
+                          intermediate_result: IntermediateResultForLearnedGrid,
+                          grid_params: LearnedGridParams,
+                          ch_axis: int = 0) -> torch.Tensor:
+    """
+    helper function to compute derivative of loss w.r.t encoding max
+    Implementation based on LSQ+ ( https://arxiv.org/pdf/2004.09576.pdf )
+
+    Inner condition ( n <= fw <= p ):
+        dq_by_dmax = (round(x/s) - x/s) / p
+    Outer condition ( fw < n ):
+        dq_by_dmax = n/p - (round(o) - o)/p
+    Outer condition ( p < fw ):
+        dq_by_dmax = 1 - (round(o) - o)/p
+
+    :param x: input
+    :param grad: gradient
+    :param intermediate_result: data carrier containing intermediate result tensors (forward result, rounding errors)
+    :param grid_params: data carrier containing parameters for learned grid (scale, offset, n, p)
+    :param ch_axis: channel axis along which sum is computed for gradient calculation
+    :return: computed derivative of loss w.r.t encoding max
+    """
+    scaling, _, n, p = grid_params.scaling, grid_params.offset, grid_params.n, grid_params.p
+    forward_result = intermediate_result.forward_result
+    rounding_error_q = intermediate_result.rounding_error_q
+    rounding_error_o = intermediate_result.rounding_error_o
+
+    dq_by_dmax = torch.where(
+        torch.le(forward_result.data, p), rounding_error_q / p, torch.ones_like(p) - rounding_error_o / p,
+    )
+    dq_by_dmax = torch.where(
+        torch.le(n, forward_result.data), dq_by_dmax, n / p - rounding_error_o / p,
+    )
+
+    dloss_by_dmax = _compute_derivative_of_loss_function(x, dq_by_dmax, grad, scaling, ch_axis)
     return dloss_by_dmax
 
 
-def compute_dloss_by_dx_using_scale_offset(x, grad, scaling, offset, n, p):
+def compute_dloss_by_dx_using_scale_offset(x: torch.Tensor,
+                                           grad: torch.Tensor,
+                                           grid_params: LearnedGridParams) -> torch.Tensor:
     """
     compute derivative w.r.t input
+    :param x: input
     :param grad: gradient
-    :param scaling: scaling factor computed for given encoding min/max
-    :param offset: offset computed
-    :param n: lower bound
-    :param p: upper bound
+    :param grid_params: data carrier containing parameters for learned grid (scale, offset, n, p)
     :return: gradient w.r.t input
     """
-
+    scaling, offset, n, p = grid_params.scaling, grid_params.offset, grid_params.n, grid_params.p
     # R(x/s) + R(o)
     r_x_by_s_plus_round_o = torch.round(x / scaling) + offset
 

TrainingExtensions/torch/test/python/test_qc_quantize_op.py
@@ -46,8 +46,8 @@ from aimet_common.defs import MAP_ROUND_MODE_TO_PYMO, QuantizationDataType
 from aimet_torch.qc_quantize_op import StaticGridQuantWrapper, LearnedGridQuantWrapper, SteGatingFuncForParameters, \
     QcQuantizeOpMode
 from aimet_torch.qc_quantize_op import QuantScheme
-from aimet_torch.quantsim_straight_through_grad import compute_dloss_by_dx_using_scale_offset,\
-    compute_dloss_by_dmin_using_dmax, compute_dloss_by_dmax
+from aimet_torch.quantsim_straight_through_grad import compute_dloss_by_dmax, compute_dloss_by_dmin, \
+    compute_dloss_by_dx_using_scale_offset, LearnedGridParams, compute_intermediate_result_for_learned_grid
 from aimet_torch.tensor_quantizer import StaticGridPerTensorQuantizer
 from aimet_torch.tensor_quantizer import LearnedGridTensorQuantizer
 import libpymo
@@ -430,16 +430,15 @@ class CustomFunc(torch.autograd.Function):
     @staticmethod
     def compute_scaling_offset(weight_min, weight_max):
         scaling = (weight_max - weight_min) / 255
-        offset = RoundStraightThrough.apply(-weight_min / scaling)
-
+        offset = torch.round(-weight_min / scaling)
         return scaling, offset
 
     @staticmethod
-    def quant_dequant_param(x, weight_min, weight_max):
-        delta = (weight_max - weight_min) / 255
-        offset = RoundStraightThrough.apply(-weight_min / delta)
-        x = x.clamp(weight_min.item(), weight_max.item())
-        x = RoundStraightThrough.apply(x / delta) + offset
+    def quant_dequant_param(x, _min, _max, n, p):
+        delta = (_max - _min) / p  # only valid n=0, p=2**bw - 1
+        offset = torch.round(-_min / delta)
+        x = torch.round(x / delta) + offset
+        x = torch.clamp(x, n, p)
         x = (x - offset) * delta
         return x
 
@@ -456,15 +455,8 @@ class CustomFunc(torch.autograd.Function):
         :return: computed quant dequant output
         """
         # save tensors for backward pass
-        ctx.save_for_backward(tensor, enc_min, enc_max)
-        scaling, offset = CustomFunc.compute_scaling_offset(enc_min, enc_max)
-        # save params for backward pass
-        ctx.offset = offset
-        ctx.scaling = scaling
-        ctx.n = n
-        ctx.p = p
-        y = CustomFunc.quant_dequant_param(tensor, enc_min, enc_max)
-
+        ctx.save_for_backward(tensor, enc_min, enc_max, n, p)
+        y = CustomFunc.quant_dequant_param(tensor, enc_min, enc_max, n, p)
         return y
 
     @staticmethod
@@ -478,15 +470,20 @@ class CustomFunc(torch.autograd.Function):
         :return: gradients w.r.t input, encoding_min and encoding_max and None for two extra params.
         """
 
-        input, weight_min, weight_max = ctx.saved_tensors
+        input, weight_min, weight_max, n, p = ctx.saved_tensors
         # print('--------------------------------')
         # print('gradient computation code ')
         # print('input, min, max passed are ', input, weight_min, weight_max)
         # print('--------------------------------')
+        scale = (weight_max - weight_min) / p  # only valid n=0, p=2**bw-1
+        offset = -weight_min / scale
 
-        grad_input = compute_dloss_by_dx_using_scale_offset(input, grad, ctx.scaling, ctx.offset, ctx.n, ctx.p)
-        grad_max = compute_dloss_by_dmax(input, grad, ctx.scaling, ctx.offset, ctx.n, ctx.p)
-        grad_min = compute_dloss_by_dmin_using_dmax(grad_max)
+        grid_params = LearnedGridParams(scale, offset, n, p)
+        intermediate_result = compute_intermediate_result_for_learned_grid(input, scale, offset)
+
+        grad_input = compute_dloss_by_dx_using_scale_offset(input, grad, grid_params)
+        grad_min = compute_dloss_by_dmin(input, grad, intermediate_result, grid_params)
+        grad_max = compute_dloss_by_dmax(input, grad, intermediate_result, grid_params)
 
         return grad_input, grad_min, grad_max, None, None
 
@@ -509,17 +506,18 @@ class TestQcQuantizeOpLearnedGrid:
         assert np.amin(np.abs(tensor.detach().numpy()), axis=(0, 1, 2, 3)) >= -5
 
     @staticmethod
-    def perform_auto_grad_computation(custom_input, min_value, max_value):
+    def perform_auto_grad_computation(custom_input, min_value, max_value, n=0., p=255.):
         """
         helper to perform auto grad computation
         :return:
         """
+        ste = RoundStraightThrough.apply
 
-        def quant_dequant_param(x, weight_min, weight_max):
-            delta = (weight_max - weight_min) / 255
-            offset = RoundStraightThrough.apply(-weight_min / delta)
-            x = x.clamp(weight_min.item(), weight_max.item())
-            x = RoundStraightThrough.apply(x / delta) + offset
+        def quant_dequant_param(x, _min, _max):
+            delta = (_max - _min) / p
+            offset = ste(-_min / delta)
+            x = ste(x / delta) + offset
+            x = torch.clamp(x, n, p)
             x = (x - offset) * delta
             return x
 
@@ -582,20 +580,26 @@ class TestQcQuantizeOpLearnedGrid:
         dtype = torch.float
         device = torch.device("cpu")
 
-        torch.manual_seed(0)
-        custom_input = torch.rand((2, 2), requires_grad=True, dtype=dtype, device=device)
+        # check if it covers all the conditions
+        _min = 0.1
+        _max = 0.9
+        data_size = (100, 100)
 
-        a_input_grad, a_min_grad, a_max_grad = TestQcQuantizeOpLearnedGrid.perform_auto_grad_computation(custom_input,
-                                                                                                         0.0015, 1.0)
+        torch.manual_seed(0)
+        custom_input = torch.rand(data_size, requires_grad=True, dtype=dtype, device=device)
+
+        a_input_grad, a_min_grad, a_max_grad = TestQcQuantizeOpLearnedGrid.perform_auto_grad_computation(
+            custom_input, _min, _max
+        )
         # custom gradient computation
         torch.manual_seed(0)
-        custom_input = torch.rand((2, 2), requires_grad=True, dtype=dtype, device=device)
+        custom_input = torch.rand(data_size, requires_grad=True, dtype=dtype, device=device)
 
         # compute this one time and pass it to forward function
         n, p = LearnedGridTensorQuantizer.get_n_and_p(bitwidth=8, use_symmetric_encoding=False)
-        c_input_grad, c_min_grad, c_max_grad = TestQcQuantizeOpLearnedGrid.perform_custom_grad_computation(custom_input,
-                                                                                                           0.0015, 1.0,
-                                                                                                           n, p)
+        c_input_grad, c_min_grad, c_max_grad = TestQcQuantizeOpLearnedGrid.perform_custom_grad_computation(
+            custom_input, _min, _max, n, p
+        )
 
         # validate gradients computed
         assert torch.allclose(c_input_grad.data[0], a_input_grad.data[0])