f8956e919ca23fa6f86a64ede9d8bfc3960a9831
Kyuyeun Kim <quic_kyuykim@quicinc.com>
2022-05-27 08:04:44 +0900

    Update get variable from op TF api to handle gather op

--- TrainingExtensions/tensorflow/test/python/non_eager/test_transformer.py
+++ TrainingExtensions/tensorflow/test/python/non_eager/test_transformer.py
@@ -61,7 +61,6 @@ if version.parse(tf.version.VERSION) >= version.parse("2.0"):
 
 @pytest.mark.tf2
 class TransformerQuantizationUnittests(unittest.TestCase):
-    @pytest.mark.skip
     def test_hf_bert(self):
         tf.compat.v1.reset_default_graph()
 
@@ -142,7 +141,99 @@ class TransformerQuantizationUnittests(unittest.TestCase):
         del sim
         sess.close()
 
-    @pytest.mark.skip
+    def test_hf_bert_all_inputs(self):
+        tf.compat.v1.reset_default_graph()
+
+        configuration = BertConfig(num_hidden_layers=1)
+        model = TFBertModel(configuration)
+
+        input_ids = tf.keras.Input([512], dtype=tf.int32, name='input_ids')
+        position_ids = tf.keras.Input([512], dtype=tf.int32, name='position_ids')
+        token_type_ids = tf.keras.Input([512], dtype=tf.int32, name='token_type_ids')
+        attention_mask = tf.keras.Input([512], dtype=tf.int32, name='attention_mask')
+        encoded = {
+            'input_ids': input_ids,
+            'position_ids': position_ids,
+            'token_type_ids': token_type_ids,
+            'attention_mask': attention_mask
+        }
+        outputs = model(encoded)
+
+        sess = tf.compat.v1.Session()
+        initialize_uninitialized_vars(sess)
+
+        sim = QuantizationSimModel(sess, [input_ids.op.name, position_ids.op.name, token_type_ids.op.name,
+                                          attention_mask.op.name], [outputs['pooler_output'].op.name], use_cuda=False)
+
+        def dummy_forward_pass(sess, args):
+            model_output = sess.graph.get_tensor_by_name('tf_bert_model/bert/pooler/dense/Tanh_quantized:0')
+            np.random.seed(0)
+            model_inputs = {
+                sess.graph.get_tensor_by_name('input_ids:0'): np.random.randint(16384, size=[1, 512]),
+                sess.graph.get_tensor_by_name('position_ids:0'): np.random.randint(256, size=[1, 512]),
+                sess.graph.get_tensor_by_name('token_type_ids:0'): np.random.randint(1, size=[1, 512]),
+                sess.graph.get_tensor_by_name('attention_mask:0'): np.random.randint(1, size=[1, 512]),
+            }
+            sess.run(model_output, model_inputs)
+
+        sim.compute_encodings(dummy_forward_pass, None)
+
+        quant_ops = {op.name: op for op in sim.session.graph.get_operations() if op.type == 'QcQuantize'}
+        quant_ops_to_check = []
+
+        # Add Embedding quant ops
+        embedding_path = 'tf_bert_model/bert/embeddings'
+        embedding_add_1_quant = ('{}/add/add_1_quantized'.format(embedding_path), True)
+        embedding_add_quant = ('{}/add/add_quantized'.format(embedding_path), True)
+        embedding_token_quant = ('{}/Gather_2_quantized'.format(embedding_path), True)
+        embedding_position_quant = ('{}/Identity_1_quantized'.format(embedding_path), True)
+        embedding_word_quant = ('{}/Gather_quantized'.format(embedding_path), True)
+
+        quant_ops_to_check += [embedding_add_1_quant, embedding_add_quant, embedding_word_quant, embedding_token_quant,
+                               embedding_position_quant]
+
+        # Add LayerNorm quant ops
+        layernorm_paths = ['tf_bert_model/bert/embeddings', 'tf_bert_model/bert/encoder/layer_._0/attention/output',
+                           'tf_bert_model/bert/encoder/layer_._0/output']
+
+        for layernorm_path in layernorm_paths:
+            output_quant_op = ('{}/LayerNorm/batchnorm/add_1_quantized'.format(layernorm_path), True)
+            beta_quant_op = ('{}/LayerNorm/batchnorm/ReadVariableOp_quantized'.format(layernorm_path), False)
+            gamma_quant_op = ('{}/LayerNorm/batchnorm/mul/ReadVariableOp_quantized'.format(layernorm_path), True)
+
+            quant_ops_to_check += [output_quant_op, beta_quant_op, gamma_quant_op]
+
+        # Add GeLU quant ops
+        gelu_path = 'tf_bert_model/bert/encoder/layer_._0/intermediate'
+        output_quant_op = ('{}/Gelu/mul_1_quantized'.format(gelu_path), True)
+
+        quant_ops_to_check += [output_quant_op]
+
+        # Add Query, Key, and Value quant ops
+        self_attention_path = 'tf_bert_model/bert/encoder/layer_._0/attention/self'
+        for dense_type in ['query', 'key', 'value']:
+            output_quant_op = ('{}/{}/BiasAdd_quantized'.format(self_attention_path, dense_type), True)
+            parameter_quant_op = ('{}/{}/Tensordot/ReadVariableOp_quantized'
+                                  .format(self_attention_path, dense_type), True)
+            bias_quant_op = ('{}/{}/BiasAdd/ReadVariableOp_quantized'.format(self_attention_path, dense_type), False)
+
+            # No need to check input_quant_op as those are output quantize ops of layer norm
+            quant_ops_to_check += [output_quant_op, parameter_quant_op, bias_quant_op]
+
+        # Check if quant op exists and check encoding
+        # Pop quantizer from quant_ops list if it is found
+        for quant_op_name, enabled in quant_ops_to_check:
+            quant_op = quant_ops.pop(quant_op_name)
+            self.assertTrue(quant_op)
+            self.assertTrue(check_encoding(quant_op, sim, enabled))
+
+        # quant_ops should not contain any LayerNorm, GeLU, and QKV quant ops as those should have been popped
+        for quant_op_name in quant_ops.keys():
+            self.assertTrue(all(x not in quant_op_name for x in ['LayerNorm', 'Gelu', 'query', 'key', 'value']))
+
+        del sim
+        sess.close()
+
     def test_export_hf_bert(self):
         tf.compat.v1.reset_default_graph()
 
@@ -304,7 +395,6 @@ class TransformerQuantizationUnittests(unittest.TestCase):
         if os.path.exists('/tmp/orig_tf_hf_bert_model'):
             os.remove('/tmp/orig_tf_hf_bert_model')
 
-    @pytest.mark.skip
     def test_custom_hw_config_hf_bert(self):
         tf.compat.v1.reset_default_graph()
 
@@ -425,7 +515,6 @@ class TransformerQuantizationUnittests(unittest.TestCase):
 
         sess.close()
 
-    @pytest.mark.skip
     def test_hf_bert_mask(self):
         tf.compat.v1.reset_default_graph()
 
@@ -607,7 +696,6 @@ class TransformerQuantizationUnittests(unittest.TestCase):
         del sim
         sess.close()
 
-    @pytest.mark.skip
     def test_hf_bert_embedding(self):
         tf.compat.v1.reset_default_graph()
 
@@ -659,7 +747,6 @@ class TransformerQuantizationUnittests(unittest.TestCase):
         del sim
         sess.close()
 
-    @pytest.mark.skip
     def test_hf_bert_embedding_custom_config(self):
         tf.compat.v1.reset_default_graph()
 
