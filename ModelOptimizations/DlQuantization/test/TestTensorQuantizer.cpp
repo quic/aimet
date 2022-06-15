caef3302707e9f0e6ffcba028d588bb9dd2fd66e
Akin Solomon <quic_akinlawo@quicinc.com>
2022-06-15 15:06:32 -0700

    Align encoding analyzer with IQuantizationAlgorithm

--- ModelOptimizations/DlQuantization/test/TestTensorQuantizer.cpp
+++ ModelOptimizations/DlQuantization/test/TestTensorQuantizer.cpp
@@ -2,7 +2,7 @@
 //
 //  @@-COPYRIGHT-START-@@
 //
-//  Copyright (c) 2020, Qualcomm Innovation Center, Inc. All rights reserved.
+//  Copyright (c) 2020 - 2022, Qualcomm Innovation Center, Inc. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are met:
@@ -89,6 +89,78 @@ TEST(TestTensorQuantizer, SanityTestCpu)
     EXPECT_NE(inputTensor.data()[0], quantizedTensor.data()[0]);
     EXPECT_NEAR(quantizedTensor.data()[0], 5.0162, 0.001);
 }
+
+
+TEST(TestTensorQuantizer, SanityTestComputeEncodingFromDataAsymmetricTFEnhanced)
+{
+    TensorQuantizer tensorQuantizer(QuantizationMode::QUANTIZATION_TF_ENHANCED, ROUND_NEAREST);
+
+    float mean   = 2;
+    float stddev = 2;
+    std::normal_distribution<float> distribution(mean, stddev);
+    std::mt19937 generator(1);
+
+    unsigned int tensorCount = 6000;
+    std::vector<float> paramTensor(tensorCount);
+
+    for (unsigned int i = 0; i < tensorCount; i++)
+    {
+        paramTensor[i] = distribution(generator);
+    }
+
+    TfEncoding encoding{};
+    tensorQuantizer.computeEncodingFromData(8, paramTensor.data(),
+                                            tensorCount, encoding, ComputationMode::COMP_MODE_CPU, false, false, false);
+
+    std::cout << "Encoding min=" << encoding.min << ", max=" << encoding.max
+              << std::endl;
+    EXPECT_NEAR(encoding.min, -6.527, 0.001);
+    EXPECT_NEAR(encoding.max, 8.884, 0.001);
+}
+
+TEST(TestTensorQuantizer, SanityTestComputeEncodingFromDataSymmetricTF)
+{
+    TensorQuantizer tensorQuantizer(QuantizationMode::QUANTIZATION_TF, ROUND_NEAREST);
+
+    float mean   = 2;
+    float stddev = 2;
+    std::normal_distribution<float> distribution(mean, stddev);
+    std::mt19937 generator(100);
+
+    unsigned int tensorCount = 6000;
+    std::vector<float> paramTensor(tensorCount);
+
+    for (int i = 0; i < tensorCount; i++)
+    {
+        paramTensor[i] = distribution(generator);
+    }
+
+    TfEncoding encoding{};
+    tensorQuantizer.computeEncodingFromData(8, paramTensor.data(),
+                                            tensorCount, encoding, ComputationMode::COMP_MODE_CPU,
+                                            true, false, true);
+
+    std::cout << "Encoding min=" << encoding.min << ", max=" << encoding.max
+              << std::endl;
+
+    float expected_max = std::max(std::abs(*std::min_element(paramTensor.begin(), paramTensor.end())),
+                                  std::abs(*std::max_element(paramTensor.begin(), paramTensor.end())));
+
+    // Min and Max will get adjusted slightly to represent an exact zero with one of the quantized values
+    // Adjustment is expected to be less than half a delta worth
+    EXPECT_NEAR(encoding.max, expected_max, encoding.delta / 2 + 1e-4);
+    EXPECT_EQ(encoding.max, -encoding.min);
+
+    // Check that the center value is absolute 0
+    EXPECT_NEAR(encoding.min + encoding.delta * (-encoding.offset), 0, 1e-7);
+
+    EXPECT_FLOAT_EQ(encoding.delta, (encoding.max - encoding.min) / 254);
+
+    // Check that offset is -127 - another check for strict symmetric encodings
+    EXPECT_NEAR(encoding.offset, -127, 0);
+    EXPECT_EQ(encoding.bw, 8);
+}
+
 
 #ifdef GPU_QUANTIZATION_ENABLED
 
