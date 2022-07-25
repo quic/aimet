63e377c950712f7332f09b9f55c37885a93b1ff6
Gopinath Rathinam <quic_grathina@quicinc.com>
2022-07-25 18:05:53 +0530

    Add support for Entropy quantization scheme

--- ModelOptimizations/DlQuantization/src/math_functions.cpp
+++ ModelOptimizations/DlQuantization/src/math_functions.cpp
@@ -474,6 +474,210 @@ std::tuple<DTYPE, DTYPE> findOriginalRange(const PDF& pdf)
     return std::tuple<DTYPE, DTYPE>(minVal, maxVal);
 }
 
+template <typename DTYPE>
+void updateTensorHistogram(const DTYPE* data, int tensorSize, ComputationMode mode_cpu_gpu, TensorProfilingParams& tpp)
+{
+    switch (mode_cpu_gpu)
+    {
+    case COMP_MODE_CPU:
+        updateTensorHistogram_cpu(data, tensorSize, tpp);
+        break;
+    case COMP_MODE_GPU:
+    {
+#ifdef GPU_QUANTIZATION_ENABLED
+        // Fall back to CPU mode.
+        DTYPE* data_h = (DTYPE*) malloc(sizeof(DTYPE) * tensorSize);
+        CudaMemCpy(data_h, data, tensorSize * sizeof(DTYPE), CudaMemcpyDirection::DEVICE_TO_HOST);
+        updateTensorHistogram_cpu(data_h, tensorSize, tpp);
+        free(data_h);
+#else
+        throw runtime_error("Not compiled for GPU mode.");
+#endif
+    }
+    break;
+    default:
+        throw runtime_error("Unknown computation mode.");
+        break;
+    }
+}
+
+/// Gen a bin number to insert \p value into the histogram which has \p nBins
+/// with \p minValue and binWidth in histogram.
+static size_t getBin(size_t nBins, float binWidth, float minValue, float value)
+{
+    size_t result = binWidth == 0 ? 0 : std::min(static_cast<size_t>((value - minValue) / binWidth), nBins - 1);
+    return result;
+}
+
+template <typename DTYPE>
+void updateTensorHistogram_cpu(const DTYPE* data, int tensorSize, TensorProfilingParams& tpp)
+{
+    double minInput = GetMin(data, tensorSize, COMP_MODE_CPU);
+    double maxInput = GetMax(data, tensorSize, COMP_MODE_CPU);
+
+    if ((minInput == 0) && (maxInput == 0))
+    {
+        // Special case, we don't have a histogram initialized, but we have a zero tensor here
+        // No point in trying to initialize the histogram using this
+        return;
+    }
+
+    // Make sure we have a non-zero range.
+    if (minInput == maxInput)
+    {
+        maxInput = std::max(maxInput, minInput + (DTYPE) 0.01);
+    }
+
+    // Check if we need to initialize the Histogram
+    if (0 == tpp.histogram.size())
+    {
+        tpp.histogram = std::vector<double>(PDF_SIZE, 0);
+        tpp.min       = minInput;
+        tpp.max       = maxInput;
+    }
+
+    // Check if we need to rescale histogram.
+    if (minInput < tpp.min || maxInput > tpp.max)
+    {
+        double newMin = std::min(minInput, tpp.min);
+        double newMax = std::max(maxInput, tpp.max);
+
+        double destBinWidth = (static_cast<double>(newMax) - newMin) / PDF_SIZE;
+        double srcBinWidth  = (static_cast<double>(tpp.max) - tpp.min) / PDF_SIZE;
+
+        std::vector<double> scaledHistogram(PDF_SIZE, 0);
+
+        for (size_t i = 0; i < PDF_SIZE; ++i)
+        {
+            if (tpp.histogram[i] == 0)
+                continue;
+
+            double srcBinBegin = tpp.min + srcBinWidth * i;
+            size_t destBin     = (srcBinBegin - newMin) / destBinWidth;
+            double destBinEnd  = newMin + destBinWidth * (destBin + 1);
+
+            double srcBinEnd = srcBinBegin + srcBinWidth;
+
+            // Calculate how much we need to redistribute.
+            double dstBinCnt =
+                std::min(static_cast<double>(round((destBinEnd - srcBinBegin) / srcBinWidth * tpp.histogram[i])),
+                            tpp.histogram[i]);
+
+            size_t newBin = getBin(PDF_SIZE, destBinWidth, newMin, srcBinBegin);
+            scaledHistogram[newBin] += dstBinCnt;
+
+            if (dstBinCnt < tpp.histogram[i])
+            {
+                size_t newBin = getBin(PDF_SIZE, destBinWidth, newMin, srcBinBegin + destBinWidth);
+                scaledHistogram[newBin] += tpp.histogram[i] - dstBinCnt;
+            }
+        }
+
+        // Copy scaled histogram back to the existing histogram.
+        for (size_t i = 0, e = scaledHistogram.size(); i < e; ++i)
+        {
+            assert(scaledHistogram[i] >= 0 && "Invalid rescaled histogram value, it must be non-negative.");
+            tpp.histogram[i] = scaledHistogram[i];
+        }
+
+        // Update global min and max.
+        tpp.min = newMin;
+        tpp.max = newMax;
+    }
+
+    float binWidth = (tpp.max - tpp.min) / PDF_SIZE;
+    // Go through all data points and add them to the histogram.
+    for (int i = 0; i < tensorSize; ++i)
+    {
+        size_t newBin = getBin(PDF_SIZE, binWidth, tpp.min, data[i]);
+        tpp.histogram[newBin] += 1;
+    }
+    tpp.iterations++;
+}
+
+std::vector<double> rescaleHistogram(const std::vector<double>& srcHist, const double srcHistMin,
+                                     const double srcHistMax, const double destHistMin, const double destHistMax)
+{
+    // If histogram is empty then return.
+    if (srcHist.size() == 0)
+    {
+        return srcHist;
+    }
+
+    // Check if we need to rescale the histogram.
+    assert(srcHistMin < srcHistMax && "Invalid source histogram min/max range!");
+    assert(destHistMin < destHistMax && "Invalid destination histogram min/max range!");
+    if ((srcHistMin == destHistMin) && (srcHistMax == destHistMax))
+    {
+        return srcHist;
+    }
+
+    // Number of histogram bins and bin widths.
+    const size_t numBins      = srcHist.size();
+    const double srcBinWidth  = (static_cast<double>(srcHistMax) - srcHistMin) / numBins;
+    const double destBinWidth = (static_cast<double>(destHistMax) - destHistMin) / numBins;
+
+    // Iterate the source bins and distribute into the destination bins.
+    std::vector<double> destHist(numBins, 0);
+    for (size_t srcBinIdx = 0; srcBinIdx < numBins; srcBinIdx++)
+    {
+        // Get current source bin value.
+        double srcBinVal = srcHist[srcBinIdx];
+        if (srcBinVal == 0)
+        {
+            continue;
+        }
+
+        // Get source bin start/stop values for this bin.
+        double srcBinStart = srcHistMin + srcBinIdx * srcBinWidth;
+        double srcBinStop  = srcHistMin + (srcBinIdx + 1) * srcBinWidth;
+
+        // Get destination bin indices (inclusive) which overlap with the current
+        // source bin.
+        double dstBinIdxStartF = std::floor((srcBinStart - destHistMin) / destBinWidth);
+        double dstBinIdxStopF  = std::ceil((srcBinStop - destHistMin) / destBinWidth);
+        size_t dstBinIdxStart  = static_cast<size_t>(std::max(dstBinIdxStartF, 0.0));
+        size_t dstBinIdxStop   = static_cast<size_t>(std::max(dstBinIdxStopF, 0.0));
+
+        // Upper saturate the destination bin indices.
+        if (dstBinIdxStart >= numBins)
+        {
+            dstBinIdxStart = numBins - 1;
+        }
+        if (dstBinIdxStop >= numBins)
+        {
+            dstBinIdxStop = numBins - 1;
+        }
+
+        // Redistribute the source bin into all the destination bins.
+        // Only integer values will be distributed.
+        double srcBinRem = srcBinVal;
+        for (size_t destBinIdx = dstBinIdxStart; destBinIdx <= dstBinIdxStop; destBinIdx++)
+        {
+            // Get destination bin start/stop values for this bin.
+            double destBinStart = destHistMin + destBinIdx * destBinWidth;
+            double destBinStop  = destHistMin + (destBinIdx + 1) * destBinWidth;
+
+            // Get source/destination overlap boundaries and ratio.
+            double overlapStart = std::max(srcBinStart, destBinStart);
+            double overlapStop  = std::min(srcBinStop, destBinStop);
+            double overlapRatio = (overlapStop - overlapStart) / srcBinWidth;
+            overlapRatio        = overlapRatio >= 0.0f ? overlapRatio : 0.0f;
+            overlapRatio        = overlapRatio <= 1.0f ? overlapRatio : 1.0f;
+
+            // Compute distribution value.
+            double distVal = std::round(overlapRatio * srcBinVal);
+            distVal        = distVal <= srcBinRem ? distVal : srcBinRem;
+
+            // Distribute value.
+            destHist[destBinIdx] += distVal;
+            srcBinRem -= distVal;
+        }
+    }
+
+    return destHist;
+}
+
 // Explicit instantiations
 template double GetMax(const double* data, int cnt, ComputationMode mode_cpu_gpu);
 
@@ -490,5 +694,11 @@ template void UpdatePdf(const float* data, int cnt, ComputationMode mode_cpu_gpu
 template std::tuple<double, double> findOriginalRange(const PDF& pdf);
 
 template std::tuple<float, float> findOriginalRange(const PDF& pdf);
+
+template void updateTensorHistogram(const double* data, int tensorSize, ComputationMode mode_cpu_gpu,
+                                    TensorProfilingParams& tpp);
+
+template void updateTensorHistogram(const float* data, int tensorSize, ComputationMode mode_cpu_gpu,
+                                    TensorProfilingParams& tpp);
 
 }   // End of namespace DlQuantization
