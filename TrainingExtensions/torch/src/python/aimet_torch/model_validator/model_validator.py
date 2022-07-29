d7065f17b46c56cb91633d9614d83851583cc592
Kevin Hsieh <quic_klhsieh@quicinc.com>
2022-07-29 16:00:33 -0700

    Add exclude layer functionality to model validator
    Signed-off-by: Kevin Hsieh <quic_klhsieh@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/model_validator/model_validator.py
+++ TrainingExtensions/torch/src/python/aimet_torch/model_validator/model_validator.py
@@ -60,22 +60,30 @@ class ModelValidator:
     def add_check(validation_check: Callable):
         """
         Add a validation check function to be used for validating the model. Validation check functions must take the
-        model and model inputs as inputs, and output True if the model passes the check, and False otherwise.
+        model, model inputs, and kwargs as inputs.
+        The validation check must output True if the model passes the check, and False otherwise.
         :param validation_check: Validation check function for validating the model.
         """
         ModelValidator._validation_checks.append(validation_check)
 
     @staticmethod
-    def validate_model(model: torch.nn.Module, model_input: Union[torch.Tensor, Tuple]) -> bool:
+    def validate_model(model: torch.nn.Module, model_input: Union[torch.Tensor, Tuple], **kwargs) -> bool:
         """
         Validate the pytorch model by running all validation check functions and returning True if all pass, False
         otherwise.
+        Keyword arguments can be used to pass specific arguments to particular validation checkers.
+        Currently supported keyword arguments:
+        layers_to_exclude: List of torch.nn.Modules to be excluded in the check for missing modules. These layers and
+        all of their sublayers will not be flagged if they do not have a corresponding Pytorch module.
+        :param model: Pytorch model to validate
+        :param model_input: Dummy input to the model
+        :return True if pytorch model is valid, False otherwise
         """
         is_valid_model = True
         failed_val_checks = set()
         for val_check in ModelValidator._validation_checks:
             logger.info('Running validator check %s', val_check)
-            val_check_result = val_check(model, model_input)
+            val_check_result = val_check(model, model_input, **kwargs)
             if not val_check_result:
                 failed_val_checks.add(val_check)
             is_valid_model = is_valid_model and val_check_result
