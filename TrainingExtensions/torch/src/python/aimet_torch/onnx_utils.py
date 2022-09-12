0f0b004cb8acba30a12aba770f167c94e00953f4
Sendil Krishna <quic_sendilk@quicinc.com>
2022-09-12 12:34:42 -0700

    add domain name to CustomMarker op to supress warning.
    Signed-off-by: Sendil Krishna <quic_sendilk@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/onnx_utils.py
+++ TrainingExtensions/torch/src/python/aimet_torch/onnx_utils.py
@@ -189,7 +189,7 @@ class CustomMarkerFunc(torch.autograd.Function):
         Magic method that helps with exporting a custom ONNX node
         """
         # Note: the attribute are listed alphabetically in onnx.NodeProto
-        return g.op('CustomMarker', inp, id_s=identifier, leaf_s=is_leaf, start_s=start)
+        return g.op('aimet_torch::CustomMarker', inp, id_s=identifier, leaf_s=is_leaf, start_s=start)
 
     @staticmethod
     def forward(ctx, inp, _identifier, _start, _is_leaf):     # pylint: disable=arguments-differ
