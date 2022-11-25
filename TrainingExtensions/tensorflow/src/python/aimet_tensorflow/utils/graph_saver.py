8905c696c7d885ae378b099fdd35c01da3483a9a
Rishabh Thakur <quic_ristha@quicinc.com>
2022-11-25 14:32:06 +0530

    Keras SVD utilities
    Signed-off-by: Rishabh Thakur <quic_ristha@quicinc.com>

--- TrainingExtensions/tensorflow/src/python/aimet_tensorflow/utils/graph_saver.py
+++ TrainingExtensions/tensorflow/src/python/aimet_tensorflow/utils/graph_saver.py
@@ -3,7 +3,7 @@
 # =============================================================================
 #  @@-COPYRIGHT-START-@@
 #
-#  Copyright (c) 2019, Qualcomm Innovation Center, Inc. All rights reserved.
+#  Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are met:
@@ -123,7 +123,7 @@ def get_meta_and_checkpoint_path(working_dir: str) -> str:
     if working_dir is not None:
 
         if not os.path.exists(working_dir):
-            raise ValueError("working_dir : {} does not exist".format(working_dir))
+            raise ValueError(f"working_dir : {working_dir} does not exist")
 
         meta_path = working_dir
 
@@ -164,3 +164,82 @@ def wrapper_func(eval_func: EvalFunction):
         return eval_func(*args, **kwargs)
 
     return save_and_reload_tf_sess
+
+
+def keras_wrapper_func(eval_func: EvalFunction):
+    """
+    :param eval_func: eval function
+    :return: wrapper function
+    """
+    # 'save_and_reload_keras_model' is a Wrapper function in which
+    # the argument - 'eval_func' is called
+    def save_and_reload_keras_model(*args, **kwargs):
+
+        # convert to list to update arg
+        args = list(args)
+
+        # if it's empty or first arg not of type tf.keras.Model, raise error message
+        if not args or not isinstance(args[0], tf.keras.Model):
+            raise ValueError('First argument to eval function should be keras model')
+
+        # In keras after making changes to the graph you must save and reload, then evaluate
+        tmp_dir = './data/saved_model'
+        model = keras_save_and_load_graph(tmp_dir, args[0])
+
+        # update the argument with new session
+        args[args.index(args[0])] = model
+
+        # convert back to tuple
+        args = tuple(args)
+
+        # returning the actual function now inside the wrapper function.
+        return eval_func(*args, **kwargs)
+
+    return save_and_reload_keras_model
+
+
+def keras_save_and_load_graph(directory_path: str, model: tf.keras.Model) -> tf.keras.Model:
+    """
+    saves and loads a keras model and returns the new model obtained.
+    :param directory_path: path to save the file
+    :param model: model to be saved and loaded back
+    :return: new model after load and save
+    """
+
+    unique_id = str(datetime.datetime.now()).replace(' ', '_')
+
+    directory_path = directory_path + "_" + unique_id
+    os.makedirs(directory_path, exist_ok=True)
+
+    tf.keras.models.save_model(model, directory_path)
+    new_model = tf.keras.models.load_model(directory_path)
+
+    # delete temp folder created
+    shutil.rmtree(directory_path)
+
+    # return new session after load and save
+    return new_model
+
+
+def keras_remove_hanging_nodes(model: tf.keras.Model) -> tf.keras.Model:
+    """
+    Removes all the hanging nodes in the keras model.
+    This method assumes that all the hanging nodes are present towards the end
+    which is the case if the model is loaded from the file.
+
+    :param model: keras model from which hanging nodes needs to be removed.
+    :return udpated keras model with hanging nodes removed
+    """
+    # Get the last layer of the model
+    last_layer = model.layers[-1]
+
+    # Check if this layer is disconnected
+    while not (len(last_layer.inbound_nodes) or len(last_layer.outbound_nodes)):
+        # pylint: disable=protected-access
+        model._layers.remove(last_layer)
+        last_layer = model.layers[-1]
+
+    # Relaod the graph so that the changes gets reflected properly
+    keras_save_and_load_graph('./data/temp', model)
+
+    return model
