47f9596040743713cf7a529c4bd6a42da8022ce2
Chetan Gulecha <quic_cgulecha@quicinc.com>
2022-12-12 12:30:09 +0530

    Removed keras compute beta function from tf adaround_loss
    Signed-off-by: Chetan Gulecha <quic_cgulecha@quicinc.com>

--- TrainingExtensions/tensorflow/src/python/aimet_tensorflow/keras/adaround/adaround_optimizer.py
+++ TrainingExtensions/tensorflow/src/python/aimet_tensorflow/keras/adaround/adaround_optimizer.py
@@ -46,6 +46,7 @@ import tensorflow.keras.backend as K
 from aimet_common.utils import AimetLogger
 from aimet_tensorflow.adaround.adaround_loss import AdaroundLoss, AdaroundHyperParameters
 from aimet_tensorflow.adaround.adaround_wrapper import AdaroundWrapper
+from adaround_loss import compute_beta
 logger = AimetLogger.get_area_logger(AimetLogger.LogAreas.Quant)
 
 BATCH_SIZE = 32
@@ -95,8 +96,7 @@ class CustomModel(keras.Model):
             warm_start = self.epoch_count < opt_params.num_iterations * opt_params.warm_start
 
             # Compute beta parameter used in regularization function using cosine decay
-            beta = AdaroundLoss.compute_beta_for_keras(opt_params.num_iterations, self.epoch_count, opt_params.beta_range,
-                                                       opt_params.warm_start)
+            beta = compute_beta(opt_params.num_iterations, self.epoch_count, opt_params.beta_range, opt_params.warm_start)
             recon_loss = AdaroundLoss.compute_recon_loss(adaround_out_tensor, orig_out_tensor, channels_index=channels_index)
             round_loss = AdaroundLoss.compute_round_loss(alpha=wrapper.alpha, reg_param=opt_params.reg_param, warm_start=tf.cast(warm_start, tf.bool), beta=beta)
             total_loss = recon_loss + round_loss
