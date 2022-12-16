Fix non-strict symmetric computation in PyTorch range learning

TrainingExtensions/torch/src/python/aimet_torch/quantsim_straight_through_grad.py
@@ -150,11 +150,9 @@ def get_computed_encodings(bitwidth: int,
     half_num_steps = num_steps / 2
 
     num_steps_tensor = constant_tensor_factory(num_steps, device)
-    # NOTE: This assumes that the use_* flags reflect true condition (regardless of the encoding_* values)
-    if use_symmetric_encodings and (not is_unsigned_symmetric):
+    if use_symmetric_encodings and not is_unsigned_symmetric:
         # signed symmetric
-        absmax = torch.max(torch.abs(encoding_min), torch.abs(encoding_max))
-        delta = absmax / constant_tensor_factory(math.floor(half_num_steps), device)
+        delta = encoding_max / constant_tensor_factory(math.floor(half_num_steps), device)
         offset = -constant_tensor_factory(math.ceil(half_num_steps), device)
     else:
         delta = (encoding_max - encoding_min) / num_steps_tensor

TrainingExtensions/torch/test/python/test_bn_fold.py
@@ -1465,8 +1465,8 @@ class TestTrainingExtensionBnFoldToScale:
 
         assert isinstance(model.bn1._module_to_wrap, torch.nn.Identity)
 
-        relu_output_encoding = model.relu1.output_quantizers[0].encoding
-        delta = float((relu_output_encoding.max - relu_output_encoding.min)/255)
+        conv2_output_encoding = model.conv2.output_quantizers[0].encoding
+        delta = float((conv2_output_encoding.max - conv2_output_encoding.min) / 255)
         assert torch.allclose(baseline_output, output_after_fold, atol=delta) # Allow 1-tick difference
         assert len(folded_pairs) == 2
 