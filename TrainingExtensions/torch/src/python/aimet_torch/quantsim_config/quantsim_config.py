339640c0f7c341ed3df42282790fcc660f77c97d
haijunz <quic_haijunz@quicinc.com>
2023-02-19 08:01:16 +0800

    back out the quantsim_config changes
    Signed-off-by: haijunz <quic_haijunz@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/quantsim_config/quantsim_config.py
+++ TrainingExtensions/torch/src/python/aimet_torch/quantsim_config/quantsim_config.py
@@ -525,25 +525,11 @@ class QuantSimConfigurator(AimetCommonQuantSimConfigurator):
         :param model_input_configs: Configuration for model inputs
         """
         input_ops = get_all_input_ops(self._conn_graph)
-
-
         for op in input_ops:
             if op.get_module() in self._named_modules_to_tensor_quantizers_dict:
                 modified_tensor_quantizers = {}
                 self._set_config_for_module(self._named_modules_to_tensor_quantizers_dict[op.get_module()],
                                             model_input_configs, modified_tensor_quantizers)
-        # ------------[filter out some inputs quantizer(i.g,those connecting to previous ops)]------------------------------
-        # pylint: disable=too-many-nested-blocks
-        for op in input_ops:
-            if op.input_ops:
-                quantizors = self._get_tensor_quantizers_for_input_true_setting(op)
-                for idx, product in enumerate(op.inputs):
-                    if not product.is_model_input:
-                        if idx < len(quantizors): #skip test_transformer_layer_after_act_replacement
-                            if quantizors[idx].enabled:
-                                quantizors[idx].enabled = False
-
-
 
     def _set_model_output_configs(self, model_output_configs: ConfigType):
         """
@@ -556,8 +542,6 @@ class QuantSimConfigurator(AimetCommonQuantSimConfigurator):
                 modified_tensor_quantizers = {}
                 self._set_config_for_module(self._named_modules_to_tensor_quantizers_dict[op.get_module()],
                                             model_output_configs, modified_tensor_quantizers)
-
-
 
     # -----------------------------------[ override support begin] --------------------------------------------- #
 
