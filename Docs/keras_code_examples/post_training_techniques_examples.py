e66f8612f07fc71f096693fa01ce6797b2558a86
Matthew Ernst <quic_ernst@quicinc.com>
2023-02-21 10:23:04 -0800

    move to replacing separable conv inside of model_preparer only
    Signed-off-by: Matthew Ernst <quic_ernst@quicinc.com>

--- Docs/keras_code_examples/post_training_techniques_examples.py
+++ Docs/keras_code_examples/post_training_techniques_examples.py
@@ -41,7 +41,7 @@ from aimet_tensorflow.keras.batch_norm_fold import fold_all_batch_norms
 from aimet_tensorflow.keras.batch_norm_fold import fold_given_batch_norms
 from aimet_tensorflow.keras.cross_layer_equalization import HighBiasFold, CrossLayerScaling
 from aimet_tensorflow.keras.cross_layer_equalization import equalize_model
-from aimet_tensorflow.keras.utils.model_transform_utils import replace_relu6_with_relu, replace_separable_conv_with_depthwise_pointwise
+from aimet_tensorflow.keras.utils.model_transform_utils import replace_relu6_with_relu
 
 
 def cross_layer_equalization_auto():
@@ -56,10 +56,9 @@ def cross_layer_equalization_auto_stepwise():
     Individual api calls to perform cross layer equalization one step at a time. Pairs to fold and
     scale are found automatically.
     1. Replace Relu6 with Relu
-    2. Replace SeparableConv2D with DepthwiseConv2D and Conv2D (pointwise)
-    3. Fold batch norms
-    4. Perform cross layer scaling
-    5. Perform high bias fold
+    2. Fold batch norms
+    3. Perform cross layer scaling
+    4. Perform high bias fold
     """
 
     # Load the model to equalize
@@ -71,17 +70,14 @@ def cross_layer_equalization_auto_stepwise():
     # 2. Fold all batch norms
     folded_pairs, model = fold_all_batch_norms(model_for_cle)
 
-    # 3. Fold all batch norms
-    folded_pairs = fold_all_batch_norms(model_for_cle)
-
     bn_dict = {}
     for conv_or_linear, bn in folded_pairs:
         bn_dict[conv_or_linear] = bn
 
-    # 4. Perform cross-layer scaling on applicable layer groups
+    # 3. Perform cross-layer scaling on applicable layer groups
     cls_set_info_list = CrossLayerScaling.scale_model(model_for_cle)
 
-    # 5. Perform high bias fold
+    # 4. Perform high bias fold
     HighBiasFold.bias_fold(cls_set_info_list, bn_dict)
 
     return model_for_cle
