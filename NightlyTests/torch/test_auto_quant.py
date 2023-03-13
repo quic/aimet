a035986308d4c55bd9c801b3f07ef6297237088c
Kyunggeun Lee <quic_kyunggeu@quicinc.com>
2023-03-13 15:43:48 +0900

    Remove unused/duplicate code
    Signed-off-by: Kyunggeun Lee <quic_kyunggeu@quicinc.com>

--- NightlyTests/torch/test_auto_quant.py
+++ NightlyTests/torch/test_auto_quant.py
@@ -50,13 +50,7 @@ from aimet_torch.auto_quant_v2 import AutoQuant
 from aimet_torch import utils
 from aimet_torch.quantsim import QuantizationSimModel
 
-from test_quantize_resnet18 import model_train
-
-two_class_image_dir = './data/tiny-imagenet-2'
-image_size = 224
-batch_size = 50
-num_workers = 1
-
+from test_quantize_resnet18 import model_train, _get_data_loader
 
 class _UnlabeledDatasetWrapper(Dataset):
     def __init__(self, dataset):
@@ -70,61 +64,6 @@ class _UnlabeledDatasetWrapper(Dataset):
         images, *_ = self._dataset[index]
         return images
 
-def _get_data_loader(partition, labeled=True, num_samples=None):
-    assert partition in ('train', 'val')
-
-    t = transforms.Compose([
-        transforms.Resize(248),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-    ])
-
-    imagenet_dataset = datasets.ImageFolder(root=os.path.join(two_class_image_dir, partition), transform=t)
-    if not labeled:
-        imagenet_dataset = _UnlabeledDatasetWrapper(imagenet_dataset)
-    if num_samples:
-        import random
-        indices = random.sample(list(range(len(imagenet_dataset))), num_samples)
-        imagenet_dataset = Subset(imagenet_dataset, indices=indices)
-    return DataLoader(imagenet_dataset, batch_size=batch_size, num_workers=num_workers)
-
-
-def save_config_file_for_per_channel_quantization():
-    quantsim_config = {
-        "defaults": {
-            "ops": {
-                "is_output_quantized": "True",
-                "is_symmetric": "False"
-            },
-            "params": {
-                "is_quantized": "True",
-                "is_symmetric": "True"
-            },
-            "per_channel_quantization": "True",
-        },
-        "params": {
-            "bias": {
-                "is_quantized": "False"
-            }
-        },
-        "op_type": {},
-        "supergroups": [
-            { "op_list": ["Conv", "Relu"] },
-            { "op_list": ["Conv", "Clip"] },
-            { "op_list": ["Add", "Relu"] },
-            { "op_list": ["Gemm", "Relu"] },
-            { "op_list": ["Conv", "BatchNormalization"] },
-            { "op_list": ["Gemm", "BatchNormalization"] },
-            { "op_list": ["ConvTranspose", "BatchNormalization"] }
-        ],
-        "model_input": {},
-        "model_output": {}
-    }
-
-    with open('./quantsim_config.json', 'w') as f:
-        json.dump(quantsim_config, f)
-
 
 class QuantizeAcceptanceTests(unittest.TestCase):
     @pytest.mark.cuda
@@ -134,8 +73,9 @@ class QuantizeAcceptanceTests(unittest.TestCase):
         model_train(model, epochs=2)
 
         dummy_input = torch.randn((1, 3, 224, 224)).cuda()
-        unlabeled_data_loader = _get_data_loader("train", labeled=False, num_samples=None)
-        val_data_loader = _get_data_loader("val", num_samples=None)
+        val_data_loader = _get_data_loader().val_loader
+        unlabeled_data_loader = DataLoader(_UnlabeledDatasetWrapper(val_data_loader.dataset),
+                                           batch_size=val_data_loader.batch_size)
 
         def eval_callback(model, _):
             correct = 0
