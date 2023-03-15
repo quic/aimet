9d9aa3b3c557c21a9318fbaa09abe8b4bdb8cc4a
Michael Tuttle <quic_mtuttle@quicinc.com>
2023-03-14 17:23:51 -0700

    Add onnx quantsim acceptance test
    Signed-off-by: Michael Tuttle <quic_mtuttle@quicinc.com>

--- NightlyTests/onnx/test_quantsim.py
+++ NightlyTests/onnx/test_quantsim.py
@@ -36,21 +36,47 @@
 #  @@-COPYRIGHT-END-@@
 # =============================================================================
 import os
-import torch
+
 import numpy as np
-from torchvision import models
-from onnx import load_model
-from aimet_onnx.quantsim import QuantizationSimModel
-from aimet_common.defs import QuantScheme
-from aimet_torch.quantsim import QuantizationSimModel as PtQuantizationSimModel
 import pytest
+import torch
+from onnx import load_model
+from torchvision import models
+
+from aimet_common.defs import QuantScheme
+from aimet_onnx.quantsim import QuantizationSimModel
+from aimet_torch.quantsim import QuantizationSimModel as PtQuantizationSimModel
+from torch_utils import _get_cifar10_data_loaders, train_cifar10
 
 WORKING_DIR = '/tmp/quantsim'
+
+image_size = 32
+batch_size = 64
+num_workers = 4
+
+
+def model_eval_onnx(session, val_loader):
+    """
+    :param model: model to be evaluated
+    :param early_stopping_iterations: if None, data loader will iterate over entire validation data
+    :return: top_1_accuracy on validation data
+    """
+
+    corr = 0
+    total = 0
+    for (i, batch) in enumerate(val_loader):
+        x, y = batch[0].numpy(), batch[1].numpy()
+        in_tensor = {'input': x}
+        out = session.run(None, in_tensor)[0]
+        corr += np.sum(np.argmax(out, axis=1) == y)
+        total += x.shape[0]
+    print(f'Accuracy: {corr / total}')
+    return corr / total
 
 
 class TestQuantizeAcceptance:
     """ Acceptance test for AIMET ONNX """
-    @pytest.mark.cuda
+
     def test_quantize_resnet18(self):
         """ Test for E2E quantization """
         np.random.seed(0)
@@ -67,7 +93,7 @@ class TestQuantizeAcceptance:
 
         # layers_to_ignore = [model.conv1]
         sim_pt = PtQuantizationSimModel(model, quant_scheme=QuantScheme.post_training_tf, default_param_bw=8,
-                                   default_output_bw=8, dummy_input=torch.as_tensor(inputs))
+                                        default_output_bw=8, dummy_input=torch.as_tensor(inputs))
 
         def dummy_forward_pass_pt(model, _):
             model.eval()
@@ -92,4 +118,40 @@ class TestQuantizeAcceptance:
 
         pytorch_forward_pass_output = model(torch.as_tensor(inputs))
         onnx_forward_pass_output = sim.session.run(None, {'input': inputs})
-        assert np.all((np.asarray(pytorch_forward_pass_output.detach().numpy()) - np.asarray(onnx_forward_pass_output)) < 0.05)
+        assert np.all(np.abs(
+            np.asarray(pytorch_forward_pass_output.detach().numpy()) - np.asarray(onnx_forward_pass_output)) < 0.05)
+
+    @pytest.mark.cuda
+    def test_quantized_accuracy(self):
+        np.random.seed(0)
+        torch.manual_seed(0)
+        model = models.resnet18(pretrained=False, num_classes=10)
+        if torch.cuda.is_available():
+            device = torch.device('cuda:0')
+            model.to(device)
+
+        train_cifar10(model, 2)
+        train_loader, val_loader = _get_cifar10_data_loaders()
+
+        torch.onnx.export(model, torch.rand(batch_size, 3, 32, 32).cuda(), os.path.join(WORKING_DIR, 'resnet18.onnx'),
+                          training=torch.onnx.TrainingMode.PRESERVE,
+                          input_names=['input'], output_names=['output'])
+
+        onnx_model = load_model(os.path.join(WORKING_DIR, 'resnet18.onnx'))
+
+        sim = QuantizationSimModel(onnx_model, quant_scheme=QuantScheme.post_training_tf, default_param_bw=8,
+                                   default_activation_bw=8, use_cuda=True)
+
+        def onnx_callback(session, iters):
+            for i, batch in enumerate(train_loader):
+                x = batch[0].detach().cpu().numpy()
+                in_tensor = {'input': x}
+                session.run(None, in_tensor)
+                if i >= iters:
+                    break
+
+        sim.compute_encodings(onnx_callback, 10)
+
+        onnx_qs_acc = model_eval_onnx(sim.session, val_loader)
+
+        assert onnx_qs_acc > 0.5
