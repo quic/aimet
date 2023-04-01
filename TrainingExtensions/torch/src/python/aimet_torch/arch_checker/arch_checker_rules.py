7a7c743df08fc9c0eb17aace51da4f0c79fe8d77
Mike Hsu <quic_muchhsu@quicinc.com>
2023-03-31 17:21:18 -0700

    Add intermediate padding check
    Signed-off-by: Mike Hsu <quic_muchhsu@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/arch_checker/arch_checker_rules.py
+++ TrainingExtensions/torch/src/python/aimet_torch/arch_checker/arch_checker_rules.py
@@ -44,10 +44,13 @@ Pattern checks should follow :param connected_graph: :return list[ops]:
 from typing import Dict, List
 import torch
 
+from aimet_common.graph_searcher import GraphSearcher
+from aimet_common.graph_pattern_matcher import PatternType
 from aimet_common.utils import AimetLogger
 
 from aimet_torch.meta.connectedgraph import ConnectedGraph
 from aimet_torch.batch_norm_fold import find_standalone_batchnorm_ops
+from aimet_torch.arch_checker.arch_checker_utils import PatternHandler
 
 logger = AimetLogger.get_area_logger(AimetLogger.LogAreas.Utils)
 
@@ -66,7 +69,7 @@ def get_pattern_check_list()-> List:
     Get a list of pattern checks.
     :return: List of pattern checks.
     """
-    return [_check_batch_norm_fold]
+    return [_check_batch_norm_fold, _check_intermediate_padding]
 
 def _check_conv_channel_32_base(node: torch.nn.Module)-> bool:
     """
@@ -111,6 +114,45 @@ def _check_batch_norm_fold(connected_graph: ConnectedGraph) -> List:
     stand_alone_bn_ops = find_standalone_batchnorm_ops(connected_graph)
 
     return list(stand_alone_bn_ops)
+
+def _check_intermediate_padding(connected_graph: ConnectedGraph) -> List:
+    """
+    Checks is there intermediate padding in conv2 sequence: [Conv -> Activation -> (Optionally) BN -> Conv].
+    Activation can be [relu, tanh, hardswish].
+    :param connected_graph: Connected_graph object.
+    :return: List of conv contains intermediate padding in connected_graph.
+    """
+    def _examine_intermediate_padding_in_op_subset(op_subset):
+        """
+        Check is there intermediate padding in the second conv in op_subset.
+        If both 1st and 2nd conv have paddings, add conv2 to inter_pad_node_list.
+        """
+        if len(op_subset) == 4:
+            conv1, _, _, conv2 = op_subset
+        else:
+            conv1, _, conv2 = op_subset
+
+        conv1_padding = sum(conv1.get_module().padding)
+        conv2_padding = sum(conv2.get_module().padding)
+        if conv1_padding and conv2_padding:
+            inter_pad_op_list.append(conv2)
+
+    _support_activation_op_type = ("Relu", "Tanh", "HardSwish")
+    _support_conv_op_type = ("Conv", "Conv2D")
+
+    inter_pad_op_list = []
+    handler = PatternHandler(_examine_intermediate_padding_in_op_subset)
+
+    patterns_with_callbacks = []
+    for _act_op_type in _support_activation_op_type:
+        for _conv_op_type in _support_conv_op_type:
+            patterns_with_callbacks.append(PatternType(pattern=[_conv_op_type, _act_op_type, "BatchNormalization", _conv_op_type], action=handler))
+            patterns_with_callbacks.append(PatternType(pattern=[_conv_op_type, _act_op_type, _conv_op_type], action=handler))
+
+    graph_searcher = GraphSearcher(connected_graph, patterns_with_callbacks)
+    graph_searcher.find_all_patterns_in_graph_apply_actions()
+
+    return inter_pad_op_list
 
 class CheckType(type):
     """ Metaclass to overwrite __instancecheck__. """
