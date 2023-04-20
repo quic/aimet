34f026b82d74745bb4e298a42772f02d39764b75
Harshita Mangal <63610745+quic-mangal@users.noreply.github.com>
2023-04-20 08:09:55 -0700

    Add C++ op for FP16 quantization (#2145)
    Signed-off-by: Harshita Mangal <quic_mangal@quicinc.com>

--- ModelOptimizations/DlQuantization/src/trim_functions.hpp
+++ ModelOptimizations/DlQuantization/src/trim_functions.hpp
@@ -53,6 +53,10 @@ template <typename DTYPE>
 void quantizeDequantize(const DTYPE* in, int cnt, const TfEncoding& encoding, DTYPE* out, ComputationMode mode_cpu_gpu,
                         RoundingMode rounding_mode);
 
+
+void quantizeDequantizeFp16ForGPU(const float* in, int cnt, float* out);
+
+
 template <typename DTYPE>
 void quantizeToFxp(const DTYPE* in, int cnt, const TfEncoding& encoding, DTYPE* out, ComputationMode mode_cpu_gpu,
                    RoundingMode rounding_mode, bool shiftToSigned);
