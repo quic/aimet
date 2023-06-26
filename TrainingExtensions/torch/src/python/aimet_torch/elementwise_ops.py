61c283557f0e043e31bacbc44997ca67cf735587
Sakshi Rai <quic_saksrai@quicinc.com>
2023-06-26 15:42:58 +0530

    Adds support for GatherND Op
    Signed-off-by: Sakshi Rai <quic_saksrai@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/elementwise_ops.py
+++ TrainingExtensions/torch/src/python/aimet_torch/elementwise_ops.py
@@ -342,6 +342,7 @@ class RoiAlign(torch.nn.Module):
         roi = torch.cat((torch.reshape(batch_indices, (batch_indices.shape[0], 1)), roi), dim=1)
         return torchvision.ops.roi_align(inp, roi, self.output_size, self.spatial_scale, self.sampling_ratio)
 
+
 class NonMaxSuppression(torch.nn.Module):
     """
     Implementation of NMS Op in the form of nn.Module
@@ -366,3 +367,49 @@ class NonMaxSuppression(torch.nn.Module):
                     res.append([index, class_index, val.detach()])
                 res = res[:(self.max_output_boxes_per_class *(index+1))]
         return torch.Tensor(res).type(torch.int64)
+
+
+class GatherNd(torch.nn.Module):
+    """ GatherNd op implementation"""
+
+    # This class is created because Pytorch as of now doesn't have support for this OP
+    def __init__(self, batch_dim: int):
+        super().__init__()
+        self.batch_dims = batch_dim
+
+    def forward(self, data: torch.Tensor, indices: torch.Tensor) -> torch.Tensor:
+        """
+        Forward-pass routine for GatherNd op
+        """
+        data_rank = len(data.shape)
+
+        assert indices.shape[-1] <= data_rank
+
+        batch_dims_shape = []
+
+        batch_dims_size = 1
+
+        for i in range(self.batch_dims):
+            batch_dims_shape.append(indices.shape[i])
+            batch_dims_size *= indices.shape[i]
+
+        output_shape = (
+            batch_dims_shape + list(indices.shape)[self.batch_dims:-1]
+            if (indices.shape[-1] == data_rank - self.batch_dims)
+            else batch_dims_shape + list(indices.shape)[self.batch_dims:-1] + list(data.shape)[self.batch_dims + indices.shape[-1]:])
+
+        output_data_buffer = []
+
+        reshaped_indices = indices.reshape(batch_dims_size, -1, indices.shape[-1])
+
+        reshaped_data = data.reshape((batch_dims_size,) + data.shape[self.batch_dims:])
+
+        for batch_dim in range(reshaped_indices.shape[0]):
+            for outer_dim in range(reshaped_indices.shape[1]):
+                gather_index = tuple(reshaped_indices[batch_dim][outer_dim])
+                output_data_buffer.append(reshaped_data[(batch_dim, *gather_index)])
+
+        if output_data_buffer[0].dim() == 0:
+            return torch.tensor(output_data_buffer).reshape(output_shape)
+        else:
+            return torch.cat(output_data_buffer).reshape(output_shape)
