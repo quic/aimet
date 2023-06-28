0d45103cd2bf9a0a5b6b084d36f46d56ad119066
Kyunggeun Lee <quic_kyunggeu@quicinc.com>
2023-06-27 19:57:49 -0700

    Support float16 in range learning
    Signed-off-by: Kyunggeun Lee <quic_kyunggeu@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/tensor_factory_utils.py
+++ TrainingExtensions/torch/src/python/aimet_torch/tensor_factory_utils.py
@@ -38,24 +38,53 @@
 """
 Tensor factory utility method
 """
-from typing import Union, Dict, Tuple
-
+import functools
 import torch
 
 
-_cache: Dict[Tuple[float, torch.device], torch.Tensor] = {}
-def constant_tensor_factory(val: float,
-                            device: Union[str, torch.device]) -> torch.Tensor:
+@functools.lru_cache
+def constant_tensor_factory(data,
+                            *,
+                            dtype=None,
+                            device=None,
+                            pin_memory=False) -> torch.Tensor:
     """
-    Factory function to generate constant tensor or return cached object
-    :param val: value to obtain corresponding torch.Tensor
-    :param device: device str ('cpu', 'cuda', ...) or torch.device
-    :return: Constant tensor
+    Factory function that returns a cached constant scalar tensor with given data.
+    This function is intentionally aligned with ``torch.tensor`` API except that
+    it doesn't take ``requires_grad`` parameter.
+
+    NOTE: The returned tensor is strictly read-only. However, currently there is no way
+          one can fundamentally prevent performing in-place oeprations to the returned
+          tensors. It is up to the callers to make sure the returned tensors are not
+          modified in-place in any cases.
+
+    :param data: Data for the return tensor.
+    :param dtype: Data type of the returned tensor.
+    :param device: Device of the returned tensor.
+    :param pin_memory: Whether to allocate the tensor to pinned memory. Valid only for CPU tensors.
+    :return: Constant read-only tensor
     """
-    if isinstance(device, str):
-        device = torch.device(device)
-    if (val, device) in _cache:
-        return _cache[(val, device)]
-    tensor = torch.tensor([val], device=device)
-    _cache[(val, device)] = tensor
-    return tensor
+    if not isinstance(data, (float, int)):
+        raise ValueError(f"Expected data to be an instance of float or int. Got {type(data)}).")
+
+    return torch.tensor(data,
+                        dtype=dtype,
+                        device=device,
+                        requires_grad=False,
+                        pin_memory=pin_memory)
+
+
+def constant_like(data, tensor):
+    """
+    Factory function that returns a cached constant scalar tensor with the same
+    dtype and device as the given tensor. If the input tensor is allocated in
+    the pinned memory, the returned tensor will be also allocated to the pinned memory.
+
+    :param data: Data for the return tensor.
+    :param tensor: Tensor which the returned tensors will copy properties from.
+    :return: Constant read-only tensor
+    """
+    return constant_tensor_factory(data,
+                                   dtype=tensor.dtype,
+                                   device=tensor.device,
+                                   pin_memory=tensor.is_pinned())
