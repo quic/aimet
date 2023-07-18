0c4d338f4687d248765e2faf2c8a296cc30dd8f0
Hitarth Mehta <quic_hitameht@quicinc.com>
2023-07-18 13:37:34 +0530

    Add support for huggingface llama
    Signed-off-by: Hitarth Mehta <quic_hitameht@quicinc.com>

--- TrainingExtensions/torch/test/python/test_mha_classifier.py
+++ TrainingExtensions/torch/test/python/test_mha_classifier.py
@@ -38,10 +38,16 @@
 
 """ Unit tests to validate mha classifier """
 
-from typing import Optional, List
+import pytest
+from typing import Optional, List, Tuple
+from packaging import version
 import torch
+import transformers
 from transformers import GPT2Config, GPT2Model
 from transformers.models.gpt2.modeling_gpt2 import GPT2Attention
+if version.parse(transformers.__version__) >= version.parse("4.28.0"):
+    from transformers import LlamaModel, LlamaConfig
+    from transformers.models.llama.modeling_llama import LlamaAttention
 
 from aimet_torch.utils import replace_modules_of_type1_using_constructor
 from aimet_torch.transformers.activation import QuantizableMultiheadAttention, create_quantizable_multihead_attention
@@ -67,21 +73,35 @@ def _create_torch_mha_pattern(embed_dim, num_heads, seq_size, batch_size) -> Lis
     torch_mha = QuantizableMultiheadAttention(embed_dim, num_heads, bias=False)
     wrapped_torch_mha = TorchMhaWrapper(torch_mha, need_weights=False)
     conn_graph = ConnectedGraph(wrapped_torch_mha, dummy_input)
-    pattern = [op.type for op in conn_graph.ordered_ops]
+    pattern = [op.type for op in conn_graph.ordered_ops if op.type not in ConnectedGraph.math_invariant_types]
     return pattern
 
 
-def _create_gpt2_mha_pattern() -> List[str]:
+def _create_gpt2_mha_pattern(config: GPT2Config) -> List[str]:
     """
     Create pattern for GPT2 MHA variant.
     A pattern is list of connected graph op types in order of occurence.
     :return: List of op types in order of occurence.
     """
-    dummy_input = torch.randn(1, 1, 768)
-    gpt2_mha = GPT2Attention(GPT2Config())
+    dummy_input = torch.randn(1, 1, config.n_embd)
+    gpt2_mha = GPT2Attention(config)
     wrapped_gpt2_mha = Gpt2MhaWrapper(gpt2_mha)
     conn_graph = ConnectedGraph(wrapped_gpt2_mha, dummy_input)
-    pattern = [op.type for op in conn_graph.ordered_ops]
+    pattern = [op.type for op in conn_graph.ordered_ops if op.type not in ConnectedGraph.math_invariant_types]
+    return pattern
+
+
+def _create_llama_mha_pattern(config: LlamaConfig) -> List[str]:
+    """
+    Create pattern for GPT2 MHA variant.
+    A pattern is list of connected graph op types in order of occurence.
+    :return: List of op types in order of occurence.
+    """
+    dummy_input = torch.randn(1, 1, config.hidden_size)
+    llama_mha = LlamaAttention(config)
+    wrapped_llama_mha = LlamaMhaWrapper(llama_mha)
+    conn_graph = ConnectedGraph(wrapped_llama_mha, dummy_input)
+    pattern = [op.type for op in conn_graph.ordered_ops if op.type not in ConnectedGraph.math_invariant_types]
     return pattern
 
 
@@ -150,28 +170,61 @@ class Gpt2MhaWrapper(torch.nn.Module):
         return tuple(outputs)
 
 
+class LlamaMhaWrapper(torch.nn.Module):
+    """
+    Llama MHA variant Wrapper which allows following:
+    1) to be torch.jit.traceable because few output atrributes might be None.
+    2) generate multiple patterns for dynamic control flow inside forward pass.
+    """
+    def __init__(self,
+                 multihead_attn,
+                 attention_mask: Optional[torch.Tensor] = None,
+                 position_ids: Optional[torch.LongTensor] = None,
+                 past_key_value: Optional[Tuple[torch.Tensor]] = None,
+                 output_attentions: bool = False,
+                 use_cache: bool = False,
+                 ):
+        super(LlamaMhaWrapper, self).__init__()
+        self.multihead_attn = multihead_attn
+        self.attention_mask = attention_mask
+        self.position_ids = position_ids
+        self.past_key_value = past_key_value
+        self.output_attentions = output_attentions
+        self.use_cache = use_cache
+
+    def forward(self, *args, **kwargs):
+        kwargs["attention_mask"] = self.attention_mask
+        kwargs["position_ids"] = self.position_ids
+        kwargs["past_key_value"] = self.past_key_value
+        kwargs["output_attentions"] = self.output_attentions
+        kwargs["use_cache"] = self.use_cache
+        outputs = self.multihead_attn(*args, **kwargs)
+        outputs = [out for out in outputs if out is not None]
+        return tuple(outputs)
+
+
 class TestMhaClassifier:
 
     def test_torch_mha_variant(self):
         """ find torch variant in given model """
         embed_dim = 128
-        num_heads = 2
+        num_heads = 4
         batch_size = 1
         seq_size = 20
-        num_encoder_layers = 6
-        num_decoder_layers = 6
+        num_encoder_layers = 2
+        num_decoder_layers = 2
         pattern = _create_torch_mha_pattern(embed_dim, num_heads, seq_size, batch_size)
-        model = torch.nn.Transformer(d_model=embed_dim, nhead=num_heads,
-                                     num_encoder_layers=num_encoder_layers,
-                                     num_decoder_layers=num_decoder_layers).eval()\
-        # replace with Quantizable MHA
-        replace_modules_of_type1_using_constructor(model,
-                                                   torch.nn.MultiheadAttention,
-                                                   create_quantizable_multihead_attention)
+
         src = torch.rand((seq_size, batch_size, embed_dim))
         tgt = torch.rand((seq_size, batch_size, embed_dim))
         dummy_input = (src, tgt)
-
+        model = torch.nn.Transformer(d_model=embed_dim, nhead=num_heads,
+                                     num_encoder_layers=num_encoder_layers,
+                                     num_decoder_layers=num_decoder_layers).eval()\
+        # replace with Quantizable MHAs
+        replace_modules_of_type1_using_constructor(model,
+                                                   torch.nn.MultiheadAttention,
+                                                   create_quantizable_multihead_attention)
         mha_info = find_mha_variant(model, dummy_input, pattern=pattern)
 
         # Verify number of found MHAs
@@ -183,10 +236,12 @@ class TestMhaClassifier:
     def test_gpt2_mha_variant(self):
         """ find GPT2 variant in given model """
         config = GPT2Config()
-        pattern = _create_gpt2_mha_pattern()
-        model = GPT2Model(config)
-        input_shape = (1, 768)
-        dummy_input = torch.randint(1, input_shape)
+        config.vocab_size = 128
+        config.n_positions = 64
+        config.n_embd = 16
+        config.n_layer = 4
+        config.n_head = 4
+        pattern = _create_gpt2_mha_pattern(config)
 
         class Wrapper(torch.nn.Module):
             """
@@ -209,8 +264,10 @@ class TestMhaClassifier:
                 outputs = [out for out in outputs if out is not None]
                 return tuple(outputs)
 
+        input_shape = (1, config.n_embd)
+        dummy_input = torch.randint(1, input_shape)
+        model = GPT2Model(config)
         wrapped_model = Wrapper(model)
-
         mha_info = find_mha_variant(wrapped_model, dummy_input, pattern=pattern)
 
         # Verify number of found MHAs
@@ -218,3 +275,48 @@ class TestMhaClassifier:
         for mha in mha_info:
             assert type(wrapped_model.get_submodule(mha.module_qualified_name)) == mha.type
             assert isinstance(wrapped_model.get_submodule(mha.module_qualified_name), GPT2Attention)
+
+    @pytest.mark.skipif(version.parse(transformers.__version__) < version.parse("4.28.0"),
+                        reason="transformers 4.28.0 version is required.")
+    def test_llama_mha_variant(self):
+        """ find llama variant in given model """
+
+        config = LlamaConfig()
+        config.vocab_size = 128
+        config.hidden_size = 16
+        config.intermediate_size = 16
+        config.num_hidden_layers = 4
+        config.num_attention_heads = 4
+        pattern = _create_llama_mha_pattern(config)
+
+        class Wrapper(torch.nn.Module):
+            """
+            Wrapper which allows
+            1) LlamaModel() returns BaseModelOutputWithPast() which has nested tuples.
+            2) LlamaModel() few output atrributes might be None.
+            """
+            def __init__(self, model):
+                super(Wrapper, self).__init__()
+                self.model = model
+
+            def forward(self, *args, **kwargs):
+                outputs = self.model(*args, **kwargs)
+                hidden_states = outputs.last_hidden_state
+                past_key_values = outputs.past_key_values
+                _ = outputs.hidden_states,
+                _= outputs.attentions,
+                outputs = (hidden_states, past_key_values)
+                outputs = [out for out in outputs if out is not None]
+                return tuple(outputs)
+
+        input_shape = (1, config.hidden_size)
+        dummy_input = torch.randint(1, input_shape)
+        model = LlamaModel(config)
+        wrapped_model = Wrapper(model)
+        mha_info = find_mha_variant(wrapped_model, dummy_input, pattern=pattern)
+
+        # Verify number of found MHAs
+        assert len(mha_info) == config.num_hidden_layers
+        for mha in mha_info:
+            assert type(wrapped_model.get_submodule(mha.module_qualified_name)) == mha.type
+            assert isinstance(wrapped_model.get_submodule(mha.module_qualified_name), LlamaAttention)
