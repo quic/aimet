3a4e6a3d0e767d694096ff0f7722baac8632c95e
Harshita Mangal <63610745+quic-mangal@users.noreply.github.com>
2023-10-10 11:23:46 -0700

    Add export logic for adaround encodings (#2499)
    Signed-off-by: Harshita Mangal <quic_mangal@quicinc.com>

---
+++ Docs/api_docs/onnx_adaround.rst
@@ -0,0 +1,47 @@
+:orphan:
+
+.. _api-onnx-adaround:
+
+==================================
+AIMET ONNX AdaRound API
+==================================
+
+User Guide Link
+================
+To learn more about this technique, please see :ref:`AdaRound<ug-adaround>`
+
+Top-level API
+=============
+.. autofunction:: aimet_onnx.adaround.adaround_weight.Adaround.apply_adaround
+
+
+Adaround Parameters
+===================
+.. autoclass:: aimet_onnx.adaround.adaround_weight.AdaroundParameters
+    :members:
+
+
+Code Example - Adaptive Rounding (AdaRound)
+===========================================
+
+This example shows how to use AIMET to perform Adaptive Rounding (AdaRound).
+
+**Required imports**
+
+.. literalinclude:: ../onnx_code_examples/adaround.py
+    :language: python
+    :lines: 42-43
+
+**User should write this function to pass calibration data**
+
+
+.. literalinclude:: ../onnx_code_examples/adaround.py
+   :language: python
+   :pyobject: pass_calibration_data
+
+
+**Apply Adaround**
+
+.. literalinclude:: ../onnx_code_examples/adaround.py
+    :language: python
+    :pyobject: apply_adaround_example
---
+++ Docs/onnx_code_examples/adaround.py
@@ -0,0 +1,68 @@
+# /usr/bin/env python3.6
+# -*- mode: python -*-
+# =============================================================================
+#  @@-COPYRIGHT-START-@@
+#
+#  Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+#  3. Neither the name of the copyright holder nor the names of its contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+#  @@-COPYRIGHT-END-@@
+# =============================================================================
+# pylint: skip-file
+
+""" AdaRound code example to be used for documentation generation. """
+
+from aimet_onnx.adaround.adaround_weight import AdaroundParameters, Adaround
+from aimet_onnx.quantsim import QuantizationSimModel
+
+
+def pass_calibration_data(model):
+    """
+    The User of the QuantizationSimModel API is expected to write this function based on their data set.
+    This is not a working function and is provided only as a guideline.
+
+    :param model:
+    """
+
+
+def apply_adaround_example(model, dataloader):
+        """
+        Example code to run adaround
+
+        """
+        params = AdaroundParameters(data_loader=dataloader, num_batches=1, default_num_iterations=5,
+                                    forward_fn=pass_calibration_data,
+                                    forward_pass_callback_args=None)
+        ada_rounded_model = Adaround.apply_adaround(model, params, './', 'dummy')
+
+        sim = QuantizationSimModel(ada_rounded_model,
+                                   default_param_bw=8,
+                                   default_activation_bw=8, use_cuda=True)
+        sim.set_and_freeze_param_encodings('./dummy.encodings')
