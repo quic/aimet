5aa5fcd2bce676940b68644bbd86518694c75ce6
Yathindra Kota <quic_ykota@quicinc.com>
2024-02-20 11:30:03 -0800

    Bug Fix: Use Multiply instead of Matmul (#2766)
    Signed-off-by: yathindra kota <quic_ykota@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/cost_calculator.py
+++ TrainingExtensions/torch/src/python/aimet_torch/cost_calculator.py
@@ -50,7 +50,7 @@ SUPPORTED_LAYER_TYPES = [torch.nn.modules.conv.Conv2d,
                          torch.nn.modules.linear.Linear,
                          torch.nn.modules.activation.Sigmoid,
                          torch.nn.modules.pooling.AvgPool2d,
-                         elementwise_ops.MatMul]
+                         elementwise_ops.Multiply]
 
 class CostCalculator(GenericCostCalculator):
     """ This is a specialized implementation of CostCalculator for PyTorch"""
