f0df49760d71079bb9d948a60ed4a7b63e3c3e19
Michael Tuttle <quic_mtuttle@quicinc.com>
2024-04-04 21:55:12 -0700

    Fix pylint failures
    Signed-off-by: Michael Tuttle <quic_mtuttle@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/experimental/v2/nn/quant_base.py
+++ TrainingExtensions/torch/src/python/aimet_torch/experimental/v2/nn/quant_base.py
@@ -208,7 +208,7 @@ class BaseQuantizationMixin(abc.ABC):
                 encoding = [encoding]
             quantizer.set_legacy_encodings(encoding)
             if freeze:
-                quantizer._freeze_encoding()
+                quantizer._freeze_encoding() # pylint:disable = protected-access
 
     def export_output_encodings(self) -> List[List[Dict]]:
         """
@@ -242,7 +242,7 @@ class BaseQuantizationMixin(abc.ABC):
                 encoding = [encoding]
             quantizer.set_legacy_encodings(encoding)
             if freeze:
-                quantizer._freeze_encoding()
+                quantizer._freeze_encoding() # pylint:disable = protected-access
 
     def export_param_encodings(self) -> Dict[str, List[Dict]]:
         """
@@ -276,7 +276,7 @@ class BaseQuantizationMixin(abc.ABC):
                 encoding = [encoding]
             quantizer.set_legacy_encodings(encoding)
             if freeze:
-                quantizer._freeze_encoding()
+                quantizer._freeze_encoding() # pylint:disable = protected-access
 
     def get_original_module(self) -> nn.Module:
         """
