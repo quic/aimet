cf41c9f67e40b1844504b256dd8f8188627a8bf3
Kevin Hsieh <quic_klhsieh@quicinc.com>
2024-04-17 10:28:07 -0700

    Add block size support in affine quantization
    Signed-off-by: Kevin Hsieh <quic_klhsieh@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/v2/quantization/affine/backends/torch_builtins.py
+++ TrainingExtensions/torch/src/python/aimet_torch/v2/quantization/affine/backends/torch_builtins.py
@@ -2,7 +2,7 @@
 # =============================================================================
 #  @@-COPYRIGHT-START-@@
 #
-#  Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
+#  Copyright (c) 2023-2024, Qualcomm Innovation Center, Inc. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are met:
@@ -35,6 +35,7 @@
 #  @@-COPYRIGHT-END-@@
 # =============================================================================
 """ Default quantization backend for quantizing weights and activations """
+from typing import Optional, List
 import torch
 
 from aimet_torch.v2.utils import _is_expandable
@@ -73,10 +74,26 @@ def _is_numerically_stable(dtype: torch.dtype, qmin: int, qmax: int):
 
 
 def _validate_arguments(tensor: torch.Tensor, scale: torch.Tensor, offset: torch.Tensor,
-                        qmin: int = None, qmax: int = None):
+                        qmin: int = None, qmax: int = None, block_size: Optional[List] = None):
     if not tensor.dtype == scale.dtype == offset.dtype:
         raise RuntimeError("Data type of tensor, scale, and offset are should be the same")
-    if not _is_expandable(scale.shape, tensor.shape):
+
+    if block_size is not None:
+        if len(scale.shape) != len(block_size):
+            raise RuntimeError(f'Length of scale shape {scale.shape} must equal length of block size {block_size}')
+        for i in range(1, len(block_size) + 1):
+            if block_size[-i] == -1:
+                # Block size is calculated based on input and encoding parameter shape
+                if tensor.shape[-i] % scale.shape[-i] != 0:
+                    raise RuntimeError(f'Each tensor dimension size for tensor shape {tensor.shape} must divide '
+                                       f'evenly with corresponding scale dimension value for scale shape {scale.shape}')
+            else:
+                if block_size[-i] * scale.shape[-i] != tensor.shape[-i]:
+                    raise RuntimeError(f'Each tensor dimension size for tensor shape {tensor.shape} must equal the '
+                                       f'corresponding scale dimension size * block size for scale shape {scale.shape} '
+                                       f'and block size {block_size}')
+
+    elif not _is_expandable(scale.shape, tensor.shape):
         raise RuntimeError(f"Scale of shape {scale.shape} cannot be expanded like input tensor of shape {tensor.shape}")
 
     if qmin is not None and qmax is not None:
@@ -85,7 +102,7 @@ def _validate_arguments(tensor: torch.Tensor, scale: torch.Tensor, offset: torch
 
 
 def quantize(tensor: torch.Tensor, scale: torch.Tensor, offset: torch.Tensor,
-             qmin: int, qmax: int) -> torch.Tensor:
+             qmin: int, qmax: int, block_size: Optional[List] = None) -> torch.Tensor:
     """
     Performs differentiable quantization given scale, offset, and quantization range.
 
@@ -94,17 +111,22 @@ def quantize(tensor: torch.Tensor, scale: torch.Tensor, offset: torch.Tensor,
     :param offset: Offset value for quantization
     :param qmin: Minimum value of the quantization range
     :param qmax: Maximum value of the quantization range
+    :param block_size: Block sizes per dimension
     """
-    _validate_arguments(tensor, scale, offset, qmin, qmax)
+    _validate_arguments(tensor, scale, offset, qmin, qmax, block_size)
 
     if not _is_range_representable(tensor.dtype, qmin, qmax):
         msg = f"{tensor.dtype} is unable to represent quantized output of range [{qmin}, {qmax}]."
         raise RuntimeError(msg)
 
-    return QuantizeFunc.apply(tensor, scale, offset, qmin, qmax)
+    orig_tensor_shape = tensor.shape
+    tensor = _reshape_tensor_for_blocks(tensor, scale, block_size)
+    scale = _reshape_encoding_param_for_blocks(scale, block_size)
+    offset = _reshape_encoding_param_for_blocks(offset, block_size)
+    return QuantizeFunc.apply(tensor, scale, offset, qmin, qmax).view(orig_tensor_shape)
 
 def quantize_dequantize(tensor: torch.Tensor, scale: torch.Tensor, offset: torch.Tensor,
-                        qmin: int, qmax: int) -> torch.Tensor:
+                        qmin: int, qmax: int, block_size: Optional[List] = None) -> torch.Tensor:
     """
     Performs differentiable quantize-dequantize given scale, offset, and quantization range.
 
@@ -113,8 +135,9 @@ def quantize_dequantize(tensor: torch.Tensor, scale: torch.Tensor, offset: torch
     :param offset: Offset value for quantization
     :param qmin: Minimum value of the quantization range
     :param qmax: Maximum value of the quantization range
+    :param block_size: Block sizes per dimension
     """
-    _validate_arguments(tensor, scale, offset, qmin, qmax)
+    _validate_arguments(tensor, scale, offset, qmin, qmax, block_size)
 
     output_dtype = internal_dtype = tensor.dtype
 
@@ -125,22 +148,32 @@ def quantize_dequantize(tensor: torch.Tensor, scale: torch.Tensor, offset: torch
         msg = f"{internal_dtype} is unable to represent quantized output of range [{qmin}, {qmax}]."
         raise RuntimeError(msg)
 
+    orig_tensor_shape = tensor.shape
+    tensor = _reshape_tensor_for_blocks(tensor, scale, block_size)
+    scale = _reshape_encoding_param_for_blocks(scale, block_size)
+    offset = _reshape_encoding_param_for_blocks(offset, block_size)
     return QuantDequantFunc.apply(tensor.to(internal_dtype),
                                   scale.to(internal_dtype),
                                   offset.to(internal_dtype),
-                                  qmin, qmax).to(output_dtype)
+                                  qmin, qmax).to(output_dtype).view(orig_tensor_shape)
 
-def dequantize(tensor: torch.Tensor, scale: torch.Tensor, offset: torch.Tensor) -> torch.Tensor:
+def dequantize(tensor: torch.Tensor, scale: torch.Tensor, offset: torch.Tensor, block_size: Optional[List] = None) \
+        -> torch.Tensor:
     """
     Performs differentiable dequantize operation given scale and offset.
 
     :param tensor: Tensor to quantize
     :param scale: Scale factor for quantization
     :param offset: Offset value for quantization
+    :param block_size: Block sizes per dimension
     :return: Resulting tensor
     """
-    _validate_arguments(tensor, scale, offset)
-    return DequantizeFunc.apply(tensor, scale, offset)
+    _validate_arguments(tensor, scale, offset, block_size=block_size)
+    orig_tensor_shape = tensor.shape
+    tensor = _reshape_tensor_for_blocks(tensor, scale, block_size)
+    scale = _reshape_encoding_param_for_blocks(scale, block_size)
+    offset = _reshape_encoding_param_for_blocks(offset, block_size)
+    return DequantizeFunc.apply(tensor, scale, offset).view(orig_tensor_shape)
 
 
 # pylint: disable=abstract-method
@@ -239,3 +272,61 @@ class QuantDequantFunc(torch.autograd.Function):
             if ctx.scale_requires_grad else None
         offset_grad = -grad * (mask * scale - scale) if ctx.offset_requires_grad else None
         return tensor_grad, scale_grad, offset_grad, None, None
+
+def _reshape_tensor_for_blocks(tensor: torch.Tensor, scale: torch.Tensor, block_size: Optional[List]) \
+        -> torch.Tensor:
+    """
+    Reshape tensor to account for block sizes. The new shape separates each dimension into num blocks and block size.
+    The resulting tensor shape has twice as many dimensions as the starting shape.
+
+    For example, given the following:
+    tensor shape: [dim_1_size, dim_2_size, dim_3_size]
+    block_size: [block_1_size, block_2_size, block_3_size]
+
+    The input is reshaped into the following expanded shape:
+    expanded shape: [dim_1_size / block_1_size, block_1_size, dim_2_size / block_2_size, block_2_size,
+                     dim_3_size / block_3_size, block_3_size]
+
+    This assumes that dimension sizes are divisible by block sizes and that no padding is required.
+    If block_size is None, the original shape is returned.
+
+    :param tensor: Tensor to reshape
+    :param scale: Encoding scale
+    :param block_size: Block sizes per dimension
+    :return: Reshaped tensor
+    """
+    if block_size is None:
+        return tensor
+
+    input_reshape = []
+    for i in range(1, len(block_size) + 1):
+        if block_size[-i] == -1:
+            input_reshape.insert(0, tensor.shape[-i] // scale.shape[-i])
+            input_reshape.insert(0, scale.shape[-i])
+        else:
+            input_reshape.insert(0, block_size[-i])
+            input_reshape.insert(0, scale.shape[-i])
+
+    input_reshape = list(tensor.shape[:-len(block_size)]) + input_reshape
+
+    return tensor.view(input_reshape)
+
+def _reshape_encoding_param_for_blocks(enc_param: torch.Tensor, block_size: Optional[List]) -> torch.Tensor:
+    """
+    Reshape encoding param to account for block sizes. If block_size is not None, the original shape is interleaved
+    with '1' in between each dimension. Otherwise, the original shape is used.
+
+    :param enc_param: Encoding param tensor to reshape
+    :param block_size: Block sizes per dimension
+    :return: Reshaped encoding parameter
+    """
+    if block_size is None:
+        return enc_param
+
+    param_reshape = []
+
+    for size in enc_param.shape:
+        param_reshape.append(size)
+        param_reshape.append(1)
+
+    return enc_param.view(param_reshape)
