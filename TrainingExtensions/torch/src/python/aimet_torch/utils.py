33e974f65307f7c6625c5a3807f977893487fd13
Rishabh Kumar Jain <quic_rij@quicinc.com>
2024-04-18 17:22:42 +0530

    Fixed import issue.
    Signed-off-by: Rishabh Kumar Jain <quic_rij@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/utils.py
+++ TrainingExtensions/torch/src/python/aimet_torch/utils.py
@@ -57,8 +57,6 @@ from aimet_common.defs import QuantScheme, QuantizationDataType, MAP_QUANT_SCHEM
 from aimet_common.utils import AimetLogger, Handle, log_with_error_and_assert_if_false
 import aimet_common.libpymo as libpymo
 from aimet_torch import elementwise_ops
-from aimet_torch.qc_quantize_op import QcQuantizeWrapper
-from aimet_torch.qc_quantize_recurrent import QcQuantizeRecurrent
 from aimet_torch.tensor_quantizer import TensorQuantizer
 
 logger = AimetLogger.get_area_logger(AimetLogger.LogAreas.Utils)
@@ -919,6 +917,9 @@ def get_all_quantizers(model: torch.nn.Module):
     :param model: Root module
     :returns: List of parameter, input, and output quantizers
     """
+    from aimet_torch.qc_quantize_op import QcQuantizeWrapper# pylint: disable=import-outside-toplevel
+    from aimet_torch.qc_quantize_recurrent import QcQuantizeRecurrent# pylint: disable=import-outside-toplevel
+
     param_quantizers = []
     input_quantizers = []
     output_quantizers = []
