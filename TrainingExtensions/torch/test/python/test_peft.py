8c7fa42753617dce001cc455701917bc0ca2f10c
Harshita Mangal <63610745+quic-mangal@users.noreply.github.com>
2024-05-13 17:25:32 -0700

    Issue add peft quant utils (#2954)
    Signed-off-by: Harshita Mangal <quic_mangal@quicinc.com>

--- TrainingExtensions/torch/test/python/test_peft.py
+++ TrainingExtensions/torch/test/python/test_peft.py
@@ -34,10 +34,18 @@
 #
 #  @@-COPYRIGHT-END-@@
 # =============================================================================
+import os
+import shutil
+import pytest
+
 import torch
-from peft.tuners.lora.layer import LoraLayer
+from safetensors.torch import save_file
+from safetensors import safe_open
+
+from peft.tuners.lora.layer import LoraLayer as PeftLoraLayer
 from peft import LoraConfig, get_peft_model
-from aimet_torch.peft import replace_lora_layers_with_quantizable_layers, track_lora_meta_data, QcLoraLayer
+from aimet_torch.peft import replace_lora_layers_with_quantizable_layers, track_lora_meta_data, LoraLayer, PeftQuantUtils
+from aimet_torch.v2.quantsim import QuantizationSimModel
 
 class DummyModel(torch.nn.Module):
     def __init__(self):
@@ -47,7 +55,6 @@ class DummyModel(torch.nn.Module):
     def forward(self, x):
         x = self.linear(x)
         return x
-
 
 def one_adapter_model():
     model = DummyModel()
@@ -81,26 +88,142 @@ class TestLoraAdapterPeft:
         model = one_adapter_model()
         count_lora_layer = 0
         for _, module in model.named_modules():
-            if isinstance(module, LoraLayer):
+            if isinstance(module, PeftLoraLayer):
                 count_lora_layer += 1
         replace_lora_layers_with_quantizable_layers(model)
         count_qc_lora_layer = 0
         new_count_lora_layer = 0
         for _, module in model.named_modules():
-            if isinstance(module, QcLoraLayer):
-                count_qc_lora_layer += 1
             if isinstance(module, LoraLayer):
+                count_qc_lora_layer += 1
+            if isinstance(module, PeftLoraLayer):
                 new_count_lora_layer += 1
         assert new_count_lora_layer == 0
         assert count_qc_lora_layer == count_lora_layer
 
     def test_track_adapter_meta_data(self):
         model = two_adapter_model()
-        meta_data = track_lora_meta_data(model)
+        replace_lora_layers_with_quantizable_layers(model)
+        meta_data = track_lora_meta_data(model, './', 'meta_data')
         assert len(meta_data) == 2
         assert 'default' in meta_data
         assert 'default_new' in meta_data
         assert meta_data['default'].alpha == 16
-        assert meta_data['default_new'].lora_b == ['base_model.model.linear.lora_B.default_new']
+        assert meta_data['default_new'].lora_B == ['base_model.model.linear.lora_B.1']
 
+    def test_freeze_base_model_params_and_activations(self):
+        model = two_adapter_model()
+        replace_lora_layers_with_quantizable_layers(model)
+        meta_data = track_lora_meta_data(model, './', 'meta_data')
 
+        dummy_inputs = torch.randn(10, 10)
+
+        def forward_pass(model, forward_pass_callback=None):
+            return model(dummy_inputs)
+
+        peft_utils = PeftQuantUtils(model, meta_data)
+
+        sim = QuantizationSimModel(model, dummy_input= dummy_inputs)
+        sim.compute_encodings(forward_pass, forward_pass_callback_args=None)
+
+        qc_lora = sim.model.base_model.model.linear
+
+        assert not _is_frozen(qc_lora.base_layer.param_quantizers['weight'])
+
+        peft_utils.freeze_base_model_param_quantizers(sim)
+
+        assert _is_frozen(qc_lora.base_layer.param_quantizers['weight'])
+        assert not _is_frozen(qc_lora.lora_A[0].param_quantizers['weight'])
+        assert not _is_frozen(qc_lora.lora_A[1].param_quantizers['weight'])
+        assert not _is_frozen(qc_lora.lora_B[0].param_quantizers['weight'])
+        assert not _is_frozen(qc_lora.lora_B[1].param_quantizers['weight'])
+
+        assert not _is_frozen(qc_lora.base_layer.output_quantizers[0])
+
+        peft_utils.freeze_base_model_activation_quantizers(sim)
+
+        assert _is_frozen(qc_lora.base_layer.output_quantizers[0])
+        assert not _is_frozen(qc_lora.lora_A[0].output_quantizers[0])
+        assert not _is_frozen(qc_lora.lora_B[1].output_quantizers[0])
+
+    def test_set_bitwidth_for_lora_adapters(self):
+        model = two_adapter_model()
+        replace_lora_layers_with_quantizable_layers(model)
+        meta_data = track_lora_meta_data(model, './', 'meta_data')
+        dummy_inputs = torch.randn(10, 10)
+
+        peft_utils = PeftQuantUtils(model, meta_data)
+
+        sim = QuantizationSimModel(model, dummy_input=dummy_inputs)
+
+        qc_lora = sim.model.base_model.model.linear
+
+        peft_utils.set_bitwidth_for_lora_adapters(sim, output_bw=4, param_bw=4)
+        assert qc_lora.base_layer.output_quantizers[0].bitwidth == 8
+        assert qc_lora.lora_A[0].param_quantizers['weight'].bitwidth == 4
+        assert qc_lora.lora_A[1].param_quantizers['weight'].bitwidth == 4
+        assert qc_lora.lora_B[1].output_quantizers[0].bitwidth == 4
+
+        peft_utils.set_bitwidth_for_given_lora_adapters(sim, 'default_new', output_bw=2, param_bw=2)
+        assert qc_lora.lora_A[0].param_quantizers['weight'].bitwidth == 4
+        assert qc_lora.lora_A[1].param_quantizers['weight'].bitwidth == 2
+        assert qc_lora.lora_B[1].output_quantizers[0].bitwidth == 2
+
+    @pytest.mark.cuda
+    def test_enable_and_load_weights_adapter(self):
+        model = one_adapter_model()
+        replace_lora_layers_with_quantizable_layers(model)
+        meta_data = track_lora_meta_data(model, './', 'meta_data')
+        dummy_inputs = torch.randn(10, 10)
+
+        peft_utils = PeftQuantUtils(model, meta_data)
+
+        sim = QuantizationSimModel(model, dummy_input=dummy_inputs)
+        qc_lora = sim.model.base_model.model.linear
+        assert torch.all(qc_lora.lora_B[0].weight == torch.zeros((10, 4)))
+
+        meta_path = './tmp'
+        if not os.path.exists(meta_path):
+            os.mkdir(meta_path)
+        tensors = {'base_model.model.linear.lora_A.0.weight': torch.randn((4, 10)),
+                   'base_model.model.linear.lora_B.0.weight': torch.randn((10, 4))}
+        path = './tmp/weight.safetensor'
+        save_file(tensors, path)
+        peft_utils.enable_adapter_and_load_weights(sim, './tmp/weight.safetensor')
+        assert torch.all(qc_lora.lora_B[0].weight == tensors['base_model.model.linear.lora_B.0.weight'])
+        shutil.rmtree('./tmp')
+
+    def test_export_adapter_weights(self):
+        model = one_adapter_model()
+
+        replace_lora_layers_with_quantizable_layers(model)
+        meta_data = track_lora_meta_data(model,'./', 'meta_data')
+        dummy_inputs = torch.randn(10, 10)
+
+        peft_utils = PeftQuantUtils(model, meta_data)
+
+        sim = QuantizationSimModel(model, dummy_input=dummy_inputs)
+        qc_lora = sim.model.base_model.model.linear
+        assert torch.all(qc_lora.lora_B[0].weight == torch.zeros((10, 4)))
+
+        meta_path = './tmp'
+        if not os.path.exists(meta_path):
+            os.mkdir(meta_path)
+
+        peft_utils.export_adapter_weights(sim, meta_path, 'weight')
+        tensor_name = []
+        with safe_open('./tmp/weight.safetensor', framework="pt", device=0) as f:
+            for key in f.keys():
+                tensor_name.append(key)
+
+        assert len(tensor_name) == 2
+        tensors = ['base_model.model.linear.lora_A.0.weight',
+                   'base_model.model.linear.lora_B.0.weight']
+        assert sorted(tensor_name) == sorted(tensors)
+        shutil.rmtree('./tmp')
+
+def _is_frozen(quantizer):
+    if quantizer._allow_overwrite == False and quantizer.min.requires_grad == False and quantizer.max.requires_grad == False:
+        return True
+    elif quantizer._allow_overwrite == True and quantizer.min.requires_grad == True and quantizer.max.requires_grad == True:
+        return False
