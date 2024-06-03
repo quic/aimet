318499c26e8c8233b77ff876fc756ec7f981577a
Kyunggeun Lee <quic_kyunggeu@quicinc.com>
2024-06-03 14:14:22 -0700

    Remove transformer clamping heuristics (#3009)
    * Remove transformer clamping heuristics from quantsim
    
    ---------
    
    Signed-off-by: Kyunggeun Lee <quic_kyunggeu@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/v2/quantsim/quantsim.py
+++ TrainingExtensions/torch/src/python/aimet_torch/v2/quantsim/quantsim.py
@@ -230,6 +230,3 @@ class QuantizationSimModel(V1QuantizationSimModel):
     @staticmethod
     def _replace_quantization_wrapper_with_native_torch_quantization_nodes(quant_sim_model, device: torch.device):
         raise NotImplementedError()
-
-    def _clamp_transformer_attention_mask_encoding(self):
-        raise NotImplementedError()
