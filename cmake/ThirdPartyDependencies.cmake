9ad4e0d67533f6850a1c2ba1832f17147914f29e
Michael Hofmann <quic_michof@quicinc.com>
2024-06-27 14:29:51 -0700

    Revert version number for GoogleTest dependency.
    Signed-off-by: Michael Hofmann <quic_michof@quicinc.com>

--- cmake/ThirdPartyDependencies.cmake
+++ cmake/ThirdPartyDependencies.cmake
@@ -102,7 +102,7 @@ else ()
     FetchContent_Declare(
         googletest
         GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG        v1.14.0
+        GIT_TAG        release-1.12.1
     )
     FetchContent_MakeAvailable(googletest)
 endif ()
