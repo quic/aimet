bb4c0fe9504fac3e0f3bb104210bf8ee3018ce44
Priyanka Dangi <quic_pdangi@quicinc.com>
2024-07-17 14:10:35 -0700

    Addressing Pylint
    Signed-off-by: Priyanka Dangi <quic_pdangi@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/v2/batch_norm_fold.py
+++ TrainingExtensions/torch/src/python/aimet_torch/v2/batch_norm_fold.py
@@ -1,9 +1,11 @@
-import torch
+"""This module enables batch norm folding in QuantSim v2"""
 from typing import List, Tuple, Iterable
+import torch
 from aimet_torch import utils
 from aimet_torch.batch_norm_fold import BatchNormFold as BatchNormFoldV1
 from aimet_torch.batch_norm_fold import _BatchNormFoldingNotSupported, LayerType, BatchNormType, _supported_batchnorms, _supported_layers
 from aimet_torch.v2.quantsim import QuantizationSimModel
+from aimet_torch.v2.nn import FakeQuantizationMixin
 from torch.nn.modules.conv import _ConvTransposeNd
 import aimet_torch.v2.quantization as Q
 from aimet_torch.v2.nn.base import BaseQuantizationMixin
@@ -11,7 +13,10 @@ from aimet_common.utils import AimetLogger
 
 _logger = AimetLogger.get_area_logger(AimetLogger.LogAreas.BatchNormFolding)
 
+
 class BatchNormFold(BatchNormFoldV1):
+    """Handles batch norm folding logic"""
+
     @staticmethod
     def fold_all_batch_norms_to_scale(sim: QuantizationSimModel) -> List[Tuple[BaseQuantizationMixin, BaseQuantizationMixin]]:
         """
@@ -28,11 +33,14 @@ class BatchNormFold(BatchNormFoldV1):
         model = sim.model
         connected_graph = sim.connected_graph
 
-        module_to_qmodule = {
-            module: getattr(sim.model, module_name)
-            for module_name, module in sim.connected_graph._name_to_module.items() if hasattr(sim.model, module_name)
-            }
-        
+        module_to_qmodule = {}
+        for module_name, module in sim.connected_graph._name_to_module.items():
+            if hasattr(sim.model, module_name):
+                module_to_qmodule[module] = getattr(sim.model, module_name)
+
+            if '.' in module_name and hasattr(sim.model, module_name.split('.')[1]):
+                module_to_qmodule[module] = getattr(sim.model, module_name.split('.')[1])
+
         conv_bn_pairs, bn_conv_pairs, _ = BatchNormFoldV1._find_all_batch_norms_to_fold(connected_graph)
 
         conv_bn_pairs = [
@@ -44,7 +52,7 @@ class BatchNormFold(BatchNormFoldV1):
 
         BatchNormFold._fold_given_batch_norms(model, conv_bn_pairs, bn_conv_pairs)
         return conv_bn_pairs + [(conv, bn) for bn, conv in bn_conv_pairs]
-    
+
     @staticmethod
     def fold_given_batch_norms(model, layer_pairs):
         """
@@ -78,7 +86,7 @@ class BatchNormFold(BatchNormFoldV1):
                 conv_bn_pairs.append((conv, bn))
 
         BatchNormFold._fold_given_batch_norms(model, conv_bn_pairs, bn_conv_pairs)
-    
+
     @staticmethod
     def _fold_given_batch_norms(model,
                                 conv_bn_pairs: Iterable[Tuple[torch.nn.Module, torch.nn.Module]],
@@ -124,7 +132,7 @@ class BatchNormFold(BatchNormFoldV1):
             for bn, conv in bn_conv_pairs:
                 _fold(conv, bn, fold_backward=False)
 
-            BatchNormFoldV1._delete_bn_from_model(model, bn_modules)
+            BatchNormFold._delete_bn_from_model(model, bn_modules)
 
     @staticmethod
     def _fold_to_scale(conv: BaseQuantizationMixin, bn: BaseQuantizationMixin):
@@ -138,21 +146,21 @@ class BatchNormFold(BatchNormFoldV1):
         output_quantizer = conv.output_quantizers[0]
 
         if output_quantizer:
-            raise BatchNormFoldV1._BatchNormFoldingNotSupported(
+            raise _BatchNormFoldingNotSupported(
                 "BatchNorm should belong to the same supergroup with the layer to be folded to."
             )
 
         if "bias" in conv.param_quantizers:
             bias_quantizer = conv.param_quantizers["bias"]
             if bias_quantizer:
-                raise BatchNormFoldV1._BatchNormFoldingNotSupported(
+                raise _BatchNormFoldingNotSupported(
                     "Can't fold BatchNorm to scale if bias quantizer is enabled."
                 )
 
         weight_quantizer = conv.param_quantizers["weight"]
 
         if isinstance(conv, _ConvTransposeNd) and conv.groups != 1:
-            raise BatchNormFoldV1._BatchNormFoldingNotSupported(
+            raise _BatchNormFoldingNotSupported(
                 "BatchNorm folding to scale is not supported for grouped ConvTransposeNd."
             )
 
@@ -162,7 +170,7 @@ class BatchNormFold(BatchNormFoldV1):
         #       as we can't guarantee this is always the case.
         #       For example, the user can manually enable quantization of batchnorms, etc...
         #       (FYI: _quantize_params takes effect only when the parameter quantizers are enabled)
-        
+
         BatchNormFold._fold_to_weight(conv, bn, fold_backward=True)
 
         gamma = bn.weight
@@ -178,7 +186,7 @@ class BatchNormFold(BatchNormFoldV1):
             else:
                 new_encoding_max[i] = weight_quantizer.min[i] * result[i]
                 new_encoding_min[i] = weight_quantizer.max[i] * result[i]
-        
+
         weight_quantizer.min.copy_(new_encoding_min)
         weight_quantizer.min.copy_(new_encoding_max)
 
@@ -189,8 +197,8 @@ class BatchNormFold(BatchNormFoldV1):
                 conv_output_quantizer = None
 
             if bn_output_quantizer:
-                if not conv_output_quantizer: 
-                    conv_output_quantizer = Q.affine.QuantizeDequantize(shape=bn_output_quantizer.shape, bitwidth=bn_output_quantizer.bitwidth, 
+                if not conv_output_quantizer:
+                    conv_output_quantizer = Q.affine.QuantizeDequantize(shape=bn_output_quantizer.shape, bitwidth=bn_output_quantizer.bitwidth,
                                                symmetric=bn_output_quantizer._symmetric)
                 conv_output_quantizer.min.copy_(bn_output_quantizer.min)
                 conv_output_quantizer.max.copy_(bn_output_quantizer.max)
@@ -199,7 +207,7 @@ class BatchNormFold(BatchNormFoldV1):
             bn_output_quantizer = None
 
         if "bias" not in conv.param_quantizers:
-            bias_quantizer = Q.affine.QuantizeDequantize(shape=weight_quantizer.shape, bitwidth=weight_quantizer.bitwidth, 
+            bias_quantizer = Q.affine.QuantizeDequantize(shape=weight_quantizer.shape, bitwidth=weight_quantizer.bitwidth,
                                                symmetric=weight_quantizer._symmetric)
             conv.param_quantizers["bias"] = bias_quantizer
 
@@ -225,14 +233,20 @@ class BatchNormFold(BatchNormFoldV1):
             conv_linear.bias = torch.nn.Parameter(bias)
 
         BatchNormFoldV1._call_py_batch_norm_fold(conv_linear.weight, conv_linear.bias, bn, fold_backward=fold_backward)
-       
+
         # Transpose weight back to N, C, H, W for transposed Conv2D, for non-depthwise layers
         if isinstance(conv_linear, torch.nn.ConvTranspose2d) and conv_linear.groups == 1:
             conv_linear.weight.data = conv_linear.weight.data.permute(1, 0, 2, 3)
+
+    @staticmethod
+    def _delete_bn_from_model(model: torch.nn.Module, bn_layer_list: Iterable[BatchNormType]):
+        quantized_identity = FakeQuantizationMixin.from_module(torch.nn.Identity())
+        utils.replace_modules_with_instances_of_new_type(model, bn_layer_list, quantized_identity)
 
 # Global variables for compatibility
 fold_all_batch_norms = BatchNormFold.fold_all_batch_norms_to_weight
 fold_all_batch_norms_to_scale = BatchNormFold.fold_all_batch_norms_to_scale
 fold_given_batch_norms = BatchNormFold.fold_given_batch_norms
+# pylint: disable=protected-access
 _is_valid_bn_fold = BatchNormFoldV1._is_valid_bn_fold
 _find_all_batch_norms_to_fold = BatchNormFoldV1._find_all_batch_norms_to_fold
