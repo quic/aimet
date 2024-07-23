b5cf7a95ec641fc0c6fe3afb12255ff622248855
Kyunggeun Lee <quic_kyunggeu@quicinc.com>
2024-07-22 22:03:47 -0700

    Resolve pylint warning
    Signed-off-by: Kyunggeun Lee <quic_kyunggeu@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/v2/nn/true_quant.py
+++ TrainingExtensions/torch/src/python/aimet_torch/v2/nn/true_quant.py
@@ -53,7 +53,7 @@ import aimet_torch.nn.modules.custom as aimet_ops
 from aimet_torch.v2.quantization.base import QuantizerBase
 from aimet_torch.v2.quantization.tensor import QuantizedTensorBase
 from aimet_torch.v2.utils import patch_attr, _ContextManager, allow_recompute
-from .base import BaseQuantizationMixin
+from .base import BaseQuantizationMixin # pylint: disable=import-error
 
 
 def _quantize_if_applicable(data: Any, quantizer: Optional[QuantizerBase]):
