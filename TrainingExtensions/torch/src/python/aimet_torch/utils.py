19aae4f613f97a7b9454170ab60bbe45a4b0d90f
Sai Chaitanya Gajula <quic_gsaichai@quicinc.com>
2024-09-03 11:06:38 +0530

    Disable torch.uint8 quantization (#3295)
    Signed-off-by: Sai Chaitanya Gajula <quic_gsaichai@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/utils.py
+++ TrainingExtensions/torch/src/python/aimet_torch/utils.py
@@ -64,7 +64,7 @@ from aimet_torch.tensor_quantizer import TensorQuantizer, StaticGridPerChannelQu
 logger = AimetLogger.get_area_logger(AimetLogger.LogAreas.Utils)
 
 dtypes_to_ignore_for_quantization = (int, float, bool, str, tuple, type(None))
-torch_dtypes_to_ignore_for_quantization = [torch.int, torch.int8, torch.int16, torch.int32, torch.int64, torch.bool]
+torch_dtypes_to_ignore_for_quantization = [torch.int, torch.int8, torch.int16, torch.int32, torch.int64, torch.bool, torch.uint8]
 allowed_output_types = (torch.Tensor, *dtypes_to_ignore_for_quantization)
 DROPOUT_TYPES = (torch.nn.Dropout, torch.nn.Dropout2d, torch.nn.Dropout3d)
 
