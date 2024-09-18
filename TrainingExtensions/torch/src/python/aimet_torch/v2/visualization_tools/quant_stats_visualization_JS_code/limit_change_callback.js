75f06824abb4e065768464cdb66b9ea055e46066
Ishan Pendse <quic_ipendse@quicinc.com>
2024-09-18 16:52:06 -0700

    Quant Stats Visualizer - Adding Box Plot visualization for Advanced Stats
    * Adding boxplots for advanced stats and modifying the backend for the same
    * Cleanup JavaScript callbacks by packaging repetitive code as a function in utils
    * Generalize the code in callbacks by adaptively iterating over columns present in datasources instead of manually listing them
    * Dynamically adjust boxplot width according to the number of boxplots
    * Update docstrings
    
    ---------
    
    Signed-off-by: Ishan Pendse <quic_ipendse@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/v2/visualization_tools/quant_stats_visualization_JS_code/limit_change_callback.js
+++ TrainingExtensions/torch/src/python/aimet_torch/v2/visualization_tools/quant_stats_visualization_JS_code/limit_change_callback.js
@@ -36,58 +36,34 @@
 
 
 // Reading values from input widgets and setting plot y axis range
-const limits_data = limits_source.data;
-limits_data['ymax'] = [parseFloat(ymax_input.value)];
-limits_data['ymin'] = [parseFloat(ymin_input.value)];
-plot.y_range.start = limits_data['ymin'][0]*1.05;
-plot.y_range.end = limits_data['ymax'][0]*1.05;
-limits_data['maxclip'] = [parseFloat(maxclip_input.value)];
-limits_data['minclip'] = [parseFloat(minclip_input.value)];
+limits_source.data['ymax'] = [parseFloat(ymax_input.value)];
+limits_source.data['ymin'] = [parseFloat(ymin_input.value)];
+plot.y_range.start = limits_source.data['ymin'][0]*1.05;
+plot.y_range.end = limits_source.data['ymax'][0]*1.05;
+limits_source.data['maxclip'] = [parseFloat(maxclip_input.value)];
+limits_source.data['minclip'] = [parseFloat(minclip_input.value)];
 
-const source_data = data_source.data;
-const idx = source_data['idx'];
-const minlist = source_data['minlist'];
-const maxlist = source_data['maxlist'];
-const namelist = source_data['namelist'];
-source_data['marker_yminlist'] = source_data['minlist'].map(t => findMax(t, limits_data['ymin'][0]));
-source_data['marker_ymaxlist'] = source_data['maxlist'].map(t => findMin(t, limits_data['ymax'][0]));
+data_source.data['marker_yminlist'] = data_source.data['minlist'].map(t => findMax(t, limits_source.data['ymin'][0]));
+data_source.data['marker_ymaxlist'] = data_source.data['maxlist'].map(t => findMin(t, limits_source.data['ymax'][0]));
 
 // Updating the filters for finding layers that cross the min or max thresholds
-min_thresh_filter.booleans = minlist.map(t => t <= limits_data['minclip'][0]);
-max_thresh_filter.booleans = maxlist.map(t => t >= limits_data['maxclip'][0]);
-
-let table_booleans;
-const table_idx = [];
-const table_namelist = [];
-const table_minlist = [];
-const table_maxlist = [];
+min_thresh_filter.booleans = data_source.data['minlist'].map(t => t <= limits_source.data['minclip'][0]);
+max_thresh_filter.booleans = data_source.data['maxlist'].map(t => t >= limits_source.data['maxclip'][0]);
 
 var view = select.value;
-if (view == "All") {
-    table_booleans = name_filter.booleans;
-} else if (view == "Min") {
-    table_booleans = booleanAnd(name_filter.booleans, min_thresh_filter.booleans);
-} else if (view == "Max") {
-    table_booleans = booleanAnd(name_filter.booleans, max_thresh_filter.booleans);
-} else if (view == "Min | Max") {
-    table_booleans = booleanAnd(name_filter.booleans, booleanOr(min_thresh_filter.booleans, max_thresh_filter.booleans));
-} else if (view == "Min & Max") {
-    table_booleans = booleanAnd(name_filter.booleans, booleanAnd(min_thresh_filter.booleans, max_thresh_filter.booleans));
+let table_booleans = process_table_view(view, name_filter, min_thresh_filter, max_thresh_filter);
+
+for (let j = 0; j < table_columns.length; j++) {
+    table_data_source.data[table_columns[j]] = [];
 }
 
-for (let i = 0; i < idx.length; i++) {
+for (let i = 0; i < data_source.data['idx'].length; i++) {
     if (table_booleans[i] == true) {
-        table_idx.push(idx[i]);
-        table_namelist.push(namelist[i]);
-        table_minlist.push(minlist[i]);
-        table_maxlist.push(maxlist[i]);
+        for (let j = 0; j < table_columns.length; j++) {
+            table_data_source.data[table_columns[j]].push(data_source.data[table_columns[j]][i]);
+        }
     }
 }
-
-table_data_source.data["idx"] = table_idx;
-table_data_source.data["namelist"] = table_namelist;
-table_data_source.data["minlist"] = table_minlist;
-table_data_source.data["maxlist"] = table_maxlist;
 
 selected_data_source.data["floor"].push(limits_source.data['ymin'][0]*1.05);
 selected_data_source.data["ceil"].push(limits_source.data['ymax'][0]*1.05);
--- TrainingExtensions/torch/src/python/aimet_torch/v2/visualization_tools/quant_stats_visualization_JS_code/reset_callback.js
+++ TrainingExtensions/torch/src/python/aimet_torch/v2/visualization_tools/quant_stats_visualization_JS_code/reset_callback.js
@@ -42,78 +42,54 @@ limits_source.data['xmax'] = default_values_source.data['default_xmax'];
 limits_source.data['xmin'] = default_values_source.data['default_xmin'];
 limits_source.data['maxclip'] = default_values_source.data['default_maxclip'];
 limits_source.data['minclip'] = default_values_source.data['default_minclip'];
-const limits_data = limits_source.data;
 
 // Resetting the plot ranges
-plot.y_range.start = limits_data['ymin'][0]*1.05;
-plot.y_range.end = limits_data['ymax'][0]*1.05;
-plot.x_range.start = limits_data['xmin'][0];
-plot.x_range.end = limits_data['xmax'][0];
+plot.y_range.start = limits_source.data['ymin'][0]*1.05;
+plot.y_range.end = limits_source.data['ymax'][0]*1.05;
+plot.x_range.start = limits_source.data['xmin'][0];
+plot.x_range.end = limits_source.data['xmax'][0];
 
 // Resetting the input widget values
-ymax_input.value = limits_data['ymax'][0].toString();
-ymin_input.value = limits_data['ymin'][0].toString();
-maxclip_input.value = limits_data['maxclip'][0].toString();
-minclip_input.value = limits_data['minclip'][0].toString();
+ymax_input.value = limits_source.data['ymax'][0].toString();
+ymin_input.value = limits_source.data['ymin'][0].toString();
+maxclip_input.value = limits_source.data['maxclip'][0].toString();
+minclip_input.value = limits_source.data['minclip'][0].toString();
 
-const source_data = data_source.data;
-const idx = source_data['idx'];
-const minlist = source_data['minlist'];
-const maxlist = source_data['maxlist'];
-const namelist = source_data['namelist'];
+data_source.data['marker_yminlist'] = data_source.data['minlist'].map(t => findMax(t, limits_source.data['ymin'][0]));
+data_source.data['marker_ymaxlist'] = data_source.data['maxlist'].map(t => findMin(t, limits_source.data['ymax'][0]));
 
-source_data['marker_yminlist'] = source_data['minlist'].map(t => findMax(t, limits_data['ymin'][0]));
-source_data['marker_ymaxlist'] = source_data['maxlist'].map(t => findMin(t, limits_data['ymax'][0]));
+min_thresh_filter.booleans = data_source.data['minlist'].map(t => t <= limits_source.data['minclip'][0]);
+max_thresh_filter.booleans = data_source.data['maxlist'].map(t => t >= limits_source.data['maxclip'][0]);
 
-min_thresh_filter.booleans = minlist.map(t => t <= limits_data['minclip'][0]);
-max_thresh_filter.booleans = maxlist.map(t => t >= limits_data['maxclip'][0]);
-
-name_filter.booleans = Array(idx.length).fill(true);
+name_filter.booleans = Array(data_source.data['idx'].length).fill(true);
 name_input.value = "";
-
-let table_booleans;
-const table_idx = [];
-const table_namelist = [];
-const table_minlist = [];
-const table_maxlist = [];
 
 select.value = "Min | Max";
 var view = select.value;
-if (view == "All") {
-    table_booleans = name_filter.booleans;
-} else if (view == "Min") {
-    table_booleans = booleanAnd(name_filter.booleans, min_thresh_filter.booleans);
-} else if (view == "Max") {
-    table_booleans = booleanAnd(name_filter.booleans, max_thresh_filter.booleans);
-} else if (view == "Min | Max") {
-    table_booleans = booleanAnd(name_filter.booleans, booleanOr(min_thresh_filter.booleans, max_thresh_filter.booleans));
-} else if (view == "Min & Max") {
-    table_booleans = booleanAnd(name_filter.booleans, booleanAnd(min_thresh_filter.booleans, max_thresh_filter.booleans));
-}
+let table_booleans = process_table_view(view, name_filter, min_thresh_filter, max_thresh_filter);
 
-for (let i = 0; i < idx.length; i++) {
+for (let i = 0; i < data_source.data['idx'].length; i++) {
     if (table_booleans[i] == true) {
-        table_idx.push(idx[i]);
-        table_namelist.push(namelist[i]);
-        table_minlist.push(minlist[i]);
-        table_maxlist.push(maxlist[i]);
+        for (let j = 0; j < table_columns.length; j++) {
+            table_data_source.data[table_columns[j]].push(data_source.data[table_columns[j]][i]);
+        }
     }
 }
-
-table_data_source.data["idx"] = table_idx;
-table_data_source.data["namelist"] = table_namelist;
-table_data_source.data["minlist"] = table_minlist;
-table_data_source.data["maxlist"] = table_maxlist;
 
 table_data_source.selected.indices = [];
 data_source.data["selected"] = Array(data_source.data["idx"].length).fill(false);
 
-selected_data_source.data["idx"] = [];
-selected_data_source.data["namelist"] = [];
 selected_data_source.data["floor"] = [];
 selected_data_source.data["ceil"] = [];
-selected_data_source.data["minlist"] = [];
-selected_data_source.data["maxlist"] = [];
+
+for (let j = 0; j < selection_columns.length; j++) {
+    selected_data_source.data[selection_columns[j]] = [];
+}
+
+if (mode == "advanced") {
+    boxplot.x_range.factors = [];
+    boxplot.width = boxplot_unit_width * 5;
+}
 
 // Emitting the changes made to ColumnDataSources
 limits_source.change.emit();
