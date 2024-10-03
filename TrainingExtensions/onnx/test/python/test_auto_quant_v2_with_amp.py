e5ff27db121a990458defadecab190f1c337338c
Kyunggeun Lee <quic_kyunggeu@quicinc.com>
2024-10-02 18:51:35 -0700

    Update copyright string
    Signed-off-by: Kyunggeun Lee <quic_kyunggeu@quicinc.com>

--- TrainingExtensions/onnx/test/python/test_auto_quant_v2_with_amp.py
+++ TrainingExtensions/onnx/test/python/test_auto_quant_v2_with_amp.py
@@ -1,26 +1,36 @@
-#!/usr/bin/env python3
 # -*- mode: python -*-
 # =============================================================================
 #  @@-COPYRIGHT-START-@@
 #
-#  Copyright 2023 Qualcomm Technologies, Inc. All rights reserved.
-#  Confidential & Proprietary - Qualcomm Technologies, Inc. ("QTI")
+#  Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
 #
-#  The party receiving this software directly from QTI (the "Recipient")
-#  may use this software as reasonably necessary solely for the purposes
-#  set forth in the agreement between the Recipient and QTI (the
-#  "Agreement"). The software may be used in source code form solely by
-#  the Recipients employees (if any) authorized by the Agreement. Unless
-#  expressly authorized in the Agreement, the Recipient may not sublicense,
-#  assign, transfer or otherwise provide the source code to any third
-#  party. Qualcomm Technologies, Inc. retains all ownership rights in and
-#  to the software
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
 #
-#  This notice supersedes any other QTI notices contained within the software
-#  except copyright notices indicating different years of publication for
-#  different portions of the software. This notice does not supersede the
-#  application of any third party copyright notice to that third partys
-#  code.
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+#  3. Neither the name of the copyright holder nor the names of its contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+#  SPDX-License-Identifier: BSD-3-Clause
 #
 #  @@-COPYRIGHT-END-@@
 # =============================================================================
